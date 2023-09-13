c11f0dd37f174dc6351332d91bd382b1a8ecb6f1
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2023-09-13 15:34:56 -0700

    placeholders

--- _utils.py
+++ _utils.py
@@ -1,1 +1,31 @@
+class track(object):
+    def __init__(self):
+        pass
+    
+    def load(self):
+        pass
 
+    def bin(self):
+        pass
+
+
+class biosample(object):
+    def __init__(self):
+        pass
+
+    def load(self):
+        pass
+
+    def preprocess(self):
+        pass
+
+    def save_preprocessed(self):
+        pass
+
+
+class augment(object):
+    def __init__(self):
+        pass 
+
+    def random_gaussian(self):
+        pass
---
+++ data.py
@@ -0,0 +1,16 @@
+class GET_DATA(object):
+    def __init__(self):
+        pass
+
+    def search_ENCODE(self, experiments, target_assembly="GRCh38"):
+        pass
+
+    def download_search_results(self):
+        pass
+
+    def save_metadata(self, metadata_file_path):
+        pass
+
+    def download_from_metadata(self, metadata_file_path):
+        pass
+
