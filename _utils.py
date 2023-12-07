0a410395b7f9036aeaf95bdd4d3fb1f788178716
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2023-12-06 16:56:03 -0800

    ...

--- _utils.py
+++ _utils.py
@@ -2,6 +2,7 @@ import os, pyBigWig, pybedtools, random, datetime, gzip, pickle
 from torch.utils.data import Dataset
 import pandas as pd
 import multiprocessing as mp
+import torch
 
 random.seed(73)
 
@@ -17,6 +18,20 @@ def get_bin_value(input_dict):
 
     return bin_value
 
+def get_bin_value_dict(input_dict):
+    if input_dict["bw_obj"] == False:
+        input_dict["bw"] = pyBigWig.open(input_dict["bw"])
+
+    bw, chr, start, end, resolution = input_dict["bw"], input_dict["chr"], input_dict["start"], input_dict["end"], input_dict["resolution"]
+    bin_value = bw.stats(chr, start, end, type="mean", nBins=(end - start) // resolution)
+
+    input_dict["signals"] = bin_value
+
+    if input_dict["bw_obj"] == False:
+        bw.close()
+
+    return input_dict
+
 class COORD(object):
     def __init__(self, Meuleman_file="data/Meuleman.tsv", cCRE_file="data/GRCh38-cCREs.bed", 
                 resolution=1000, chr_sizes_file="data/hg38.chrom.sizes", outdir="data/"):
@@ -28,6 +43,7 @@ class COORD(object):
         self.chr_sizes_file = chr_sizes_file    
 
         main_chrs = ["chr" + str(x) for x in range(1,23)] + ["chrX"]
+        main_chrs.remove("chr21") # reserved for validation
         self.chr_sizes = {}
 
         with open(self.chr_sizes_file, 'r') as f:
@@ -117,6 +133,7 @@ class PROCESS_EIC_DATA(object):
         self.max_len = max_len * self.resolution #converts max_len from #bins to #bp
         self.util = COORD(resolution=self.resolution, Meuleman_file="_", outdir=self.path)
         self.genomesize = sum(list(self.util.chr_sizes.values()))
+        self.all_assays = ['M{:02d}'.format(i) for i in range(1, 36)]
 
         self.biosamples = {}
         for f in os.listdir(self.path):
@@ -126,7 +143,7 @@ class PROCESS_EIC_DATA(object):
                     
                 self.biosamples[f[:3]][f[3:6]] = pyBigWig.open(self.path + "/" + f)
 
-    def generate_m_samples(self, m, multi_p=True, n_p=20): # m per biosample           
+    def pkl_generate_m_samples(self, m, multi_p=True, n_p=20): # m per biosample           
         if self.stratified:
             self.util.get_foreground()
             df = self.util.foreground
@@ -230,7 +247,141 @@ class PROCESS_EIC_DATA(object):
             with open(file_path, 'wb') as f:
                 pickle.dump(bios_data, f)
             os.system(f"gzip {file_path}")
-        
+
+    def generate_m_samples(self, m, n_datasets=50, multi_p=True, n_p=20):
+        if self.stratified:
+            self.util.get_foreground()
+            df = self.util.foreground
+            df = df[df["chrom"].isin(self.util.chr_sizes.keys())]
+            m_regions = []
+            used_regions = {chr: [] for chr in df['chrom'].unique()}
+
+            # Sort the DataFrame by chromosome and start position
+            df = df.sort_values(['chrom', 'start'])
+
+            # Select m/2 regions from the DataFrame
+            for _ in range(m // 2):
+                while True:
+                    # Select a random row from the DataFrame
+                    row = df.sample(1).iloc[0]
+
+                    # Generate a start position that is divisible by self.resolution and within the region
+                    rand_start = random.randint(row['start'] // self.resolution, (row['end']) // self.resolution) * self.resolution
+                    rand_end = rand_start + self.max_len
+
+
+                    # Check if the region overlaps with any existing region in the same chromosome
+                    if rand_start >= 0 and rand_end <= self.util.chr_sizes[row['chrom']]:
+                        if not any(start <= rand_end and end >= rand_start for start, end in used_regions[row['chrom']]):
+                            m_regions.append([row['chrom'], rand_start, rand_end])
+                            used_regions[row['chrom']].append((rand_start, rand_end))
+                            break
+                        
+            # Select m/2 regions that are not necessarily in the DataFrame 
+            for chr, size in self.util.chr_sizes.items():
+                m_c = int((m // 2) * (size / self.genomesize))  # Calculate the number of instances from each chromosome proportional to its size
+                for _ in range(m_c):
+                    while True:
+                        # Generate a random start position that is divisible by self.resolution
+                        rand_start = random.randint(0, (size - self.max_len) // self.resolution) * self.resolution
+                        rand_end = rand_start + self.max_len
+
+                        # Check if the region overlaps with any existing region
+                        if not any(start <= rand_end and end >= rand_start for start, end in used_regions[chr]):
+                            m_regions.append([chr, rand_start, rand_end])
+                            used_regions[chr].append((rand_start, rand_end))
+                            break
+
+        else:
+            m_regions = []
+            used_regions = {chr: [] for chr in self.util.chr_sizes.keys()}
+
+            for chr, size in self.util.chr_sizes.items():
+                m_c = int(m * (size / self.genomesize))
+
+                for _ in range(m_c):
+                    while True:
+                        # Generate a random start position that is divisible by self.resolution
+                        rand_start = random.randint(0, (size - self.max_len) // self.resolution) * self.resolution
+                        rand_end = rand_start + self.max_len
+
+                        # Check if the region overlaps with any existing region in the same chromosome
+                        if not any(start <= rand_end and end >= rand_start for start, end in used_regions[chr]):
+                            m_regions.append([chr, rand_start, rand_end])
+                            used_regions[chr].append((rand_start, rand_end))
+                            break
+
+        if multi_p:
+            bw_obj = False
+            # rewrite biosample-assay dirs instead of obj
+            self.biosamples = {}
+            for f in os.listdir(self.path):
+                if ".bigwig" in f: 
+                    if f[:3] not in self.biosamples.keys():
+                        self.biosamples[f[:3]] = {}
+                        
+                    self.biosamples[f[:3]][f[3:6]] = self.path + "/" + f
+        else:
+            bw_obj = True
+
+        ds_number = 0
+        samples_per_ds = m // n_datasets
+        for ds_i in range(0, m, samples_per_ds):
+            ds_number += 1
+
+            ds_i_regions = m_regions[ds_i : (ds_i + samples_per_ds)]
+            ds_i_regions.sort(key=lambda x: x[1]) # sorted based on start coord
+            
+            all_samples_tensor = []
+
+            for bios in self.biosamples.keys():
+                bios_data = {}
+
+                for assay in self.all_assays:
+                    bios_data[assay] = []
+
+                    if assay in self.biosamples[bios].keys(): # if available
+                        bw = self.biosamples[bios][assay]
+                        bw_query_dicts = []
+
+                        for r in ds_i_regions:
+                            bw_query_dicts.append({"bw":bw, "chr":r[0], "start":r[1], "end":r[2], "resolution": self.resolution, "bw_obj":bw_obj})
+                        
+                        if multi_p:
+                            with mp.Pool(n_p) as p:
+                                outs = p.map(get_bin_value_dict, bw_query_dicts)
+                        else:
+                            outs = []
+                            for ii in range(len(bw_query_dicts)):
+                                outs.append(get_bin_value(bw_query_dicts[ii]))
+
+                        outs.sort(key=lambda x: x['start']) # assert is sorted based on start coord
+                        m_signals = [o["signals"] for o in outs]
+                        
+                        for sample in m_signals:
+                            bios_data[assay].append(sample)
+
+                    else: # if missing
+                        for r in ds_i_regions:
+                            bios_data[assay].append([-1 for _ in range(L)])
+                
+                # Convert bios_data to a numpy array
+                bios_data_array = np.array([bios_data[assay] for assay in self.all_assays])
+
+                # Add bios_data_array to all_samples
+                all_samples_tensor.append(bios_data_array)
+
+            # Convert all_samples to a numpy array
+            all_samples_tensor = np.array(all_samples_tensor)
+
+            # Convert all_samples_array to a PyTorch tensor
+            all_samples_tensor = torch.from_numpy(all_samples_tensor)
+
+            # Ensure the tensor is of type float
+            all_samples_tensor = all_samples_tensor.float()
+
+            print(all_samples_tensor.shape)
+
     def load_m_regions(self, file_path):
         # Open the gzip file
         with gzip.open(file_path, 'rb') as f:
@@ -317,44 +468,49 @@ class augment(object):
         pass
 
 if __name__ == "__main__":
-    solar_path = "/project/compbio-lab/EIC/"
-    sample = "/project/compbio-lab/EIC/training_data/C01_m2000_25bp.pkl.gz"
-    traineic = PROCESS_EIC_DATA(solar_path+"training_data/", stratified=True)
-    m_regions = traineic.load_m_regions(sample)
+    # solar_path = "/project/compbio-lab/EIC/"
+    # sample = "/project/compbio-lab/EIC/training_data/C01_m2000_25bp.pkl.gz"
+    # traineic = PROCESS_EIC_DATA(solar_path+"training_data/", stratified=True)
+    # m_regions = traineic.load_m_regions(sample)
 
-    valeic = PROCESS_EIC_DATA(solar_path+"validation_data/", stratified=True)
-    blindeic = PROCESS_EIC_DATA(solar_path+"blind_data/", stratified=True)
+    # valeic = PROCESS_EIC_DATA(solar_path+"validation_data/", stratified=True)
+    # blindeic = PROCESS_EIC_DATA(solar_path+"blind_data/", stratified=True)
 
-    valeic.generate_m_samples_from_predefined_regions(m_regions=m_regions, multi_p=True, n_p=20)
-    blindeic.generate_m_samples_from_predefined_regions(m_regions=m_regions, multi_p=True, n_p=20)
+    # valeic.generate_m_samples_from_predefined_regions(m_regions=m_regions, multi_p=True, n_p=20)
+    # blindeic.generate_m_samples_from_predefined_regions(m_regions=m_regions, multi_p=True, n_p=20)
 
-    exit()
+    # exit()
+    # solar_path = "/project/compbio-lab/EIC/training_data/"
+    # # solar_path = "data/test/"
+    # eic = PROCESS_EIC_DATA(solar_path, stratified=True)
+    # t0 = datetime.datetime.now()
+    # # m_regions = eic.load_m_regions("data/test/C02_m500_25bp.pkl.gz")
+    # # print(len(m_regions))
+    # eic.generate_m_samples(m=2000, multi_p=True)
+    # t1 = datetime.datetime.now()
+    # # print("generated training datasets in :", t1-t0)
+    # exit()
+
     solar_path = "/project/compbio-lab/EIC/training_data/"
-    # solar_path = "data/test/"
     eic = PROCESS_EIC_DATA(solar_path, stratified=True)
     t0 = datetime.datetime.now()
-    # m_regions = eic.load_m_regions("data/test/C02_m500_25bp.pkl.gz")
-    # print(len(m_regions))
-    eic.generate_m_samples(m=2000, multi_p=True)
-    t1 = datetime.datetime.now()
-    # print("generated training datasets in :", t1-t0)
-    exit()
-
-    solar_path = "/project/compbio-lab/EIC/validation_data/"
-    # solar_path = "data/validation_data/"
-    eic = PROCESS_EIC_DATA(solar_path, stratified=True)
-    t0 = datetime.datetime.now()
-    eic.generate_m_samples(m=2000, multi_p=True)
+    eic.generate_m_samples(m=10, n_datasets=2, multi_p=False)
     t1 = datetime.datetime.now()
     print("generated training datasets in :", t1-t0)
+    
+    # solar_path = "/project/compbio-lab/EIC/validation_data/"
+    # eic = PROCESS_EIC_DATA(solar_path, stratified=True)
+    # t0 = datetime.datetime.now()
+    # eic.generate_m_samples(m=2000, multi_p=True)
+    # t1 = datetime.datetime.now()
+    # print("generated validation datasets in :", t1-t0)
 
-    solar_path = "/project/compbio-lab/EIC/blind_data/"
-    # solar_path = "data/blind_data/"
-    eic = PROCESS_EIC_DATA(solar_path, stratified=True)
-    t0 = datetime.datetime.now()
-    eic.generate_m_samples(m=2000, multi_p=True)
-    t1 = datetime.datetime.now()
-    print("generated training datasets in :", t1-t0)
+    # solar_path = "/project/compbio-lab/EIC/blind_data/"
+    # eic = PROCESS_EIC_DATA(solar_path, stratified=True)
+    # t0 = datetime.datetime.now()
+    # eic.generate_m_samples(m=2000, multi_p=True)
+    # t1 = datetime.datetime.now()
+    # print("generated blind datasets in :", t1-t0)
     exit()
     # c = COORD(resolution=25, Meuleman_file="_")
     # c.init_bins()
