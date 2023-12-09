2de0eb1d5f7ca4e5759de403f7a3dc3a5fbd7dee
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2023-12-08 18:52:46 -0800

    ...

--- model.py
+++ model.py
@@ -1022,7 +1022,7 @@ if __name__ == "__main__":
             "mask_percentage": 0.15,
             "chunk": True,
             "context_length": 1600,
-            "batch_size": 20,
+            "batch_size": 10,
             "learning_rate": 0.001
         }
 
