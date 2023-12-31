838e4d55ceaad34ae70b9172c6094892a52348ad
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2023-12-31 12:46:24 -0800

    ...

--- model.py
+++ model.py
@@ -1077,7 +1077,7 @@ def train_epidenoise17(hyper_parameters, checkpoint_path=None, start_ds=0):
         input_dim=input_dim, nhead=nhead, d_model=d_model, nlayers=nlayers, 
         output_dim=output_dim, dropout=dropout, context_length=context_length)
 
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10000, gamma=0.75)
 
     # Load from checkpoint if provided
@@ -1143,7 +1143,7 @@ if __name__ == "__main__":
         "chunk": True,
         "context_length": 400,
         "batch_size": 50,
-        "learning_rate": 0.05,
+        "learning_rate": 0.005,
         "alpha":0.75
     }
 
