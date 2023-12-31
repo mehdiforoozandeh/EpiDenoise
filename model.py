3297483b9e33ef0eddcbe57b92caf4f92708bae1
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2023-12-31 11:28:49 -0800

    ...

--- model.py
+++ model.py
@@ -786,9 +786,12 @@ class PRE_TRAINER(object):
 
                             # Masking a subset of the input data -- genomic position mask
                             masked_x_batch, cloze_mask = mask_data15(x_batch, mask_value=-1, chunk=chunk, n_chunks=n_chunks, mask_percentage=mask_percentage)
-                            
+                            print(cloze_mask.sum())
                             union_mask = cloze_mask | missing_mask_batch
+                            print(cloze_mask.sum())
                             pred_mask = cloze_mask & ~missing_mask_batch
+                            print(cloze_mask.sum())
+                            exit()
 
                             if len(available_assays_ind) > 1:
                                 assaymask_ind = random.choice(available_assays_ind)
