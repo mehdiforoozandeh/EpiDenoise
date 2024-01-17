92a0b72e572a16c276d0c7f0cee12bac26617c89
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-01-17 13:34:19 -0800

    ...

--- model.py
+++ model.py
@@ -142,8 +142,8 @@ class ComboLoss16(nn.Module):
         self.bce_loss = nn.BCELoss(reduction='mean')
 
     def forward(self, pred_signals, true_signals, pred_adjac, true_adjac, pred_mask, cloze_mask, union_mask):
-        mse_obs_loss = self.mse_loss(pred_signals[~union_mask], true_signals[~union_mask])
-        mse_pred_loss = self.mse_loss(pred_signals[cloze_mask], true_signals[cloze_mask])
+        mse_obs_loss =  torch.sqrt(self.mse_loss(pred_signals[~union_mask], true_signals[~union_mask]))
+        mse_pred_loss = torch.sqrt(self.mse_loss(pred_signals[cloze_mask], true_signals[cloze_mask]))
 
         # Check for nan values in pred_adjac and true_adjac
         if torch.isnan(pred_adjac).any() or torch.isnan(true_adjac).any() or torch.isnan(pred_mask).any():
