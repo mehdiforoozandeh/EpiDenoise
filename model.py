8dfc0f32439d227e03c16c95821283ba9f0d145e
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-01-23 16:55:09 -0800

    ...

--- model.py
+++ model.py
@@ -652,7 +652,8 @@ class EpiDenoise20(nn.Module):
         self.convtower = nn.Sequential([
             ConvTower(
                 d_model/(n_cnn_layer - (i+1)), 
-                d_model/(n_cnn_layer - (i))) for i in range(n_cnn_layer)
+                d_model/(n_cnn_layer - (i))) for i in range(n_cnn_layer),
+                kernel_size/(i+1), 1, dilation
             ])
 
         # self.convblock1 =   ConvBlock(2*input_dim, d_model, kernel_size, 1, 1)
@@ -2410,6 +2411,8 @@ if __name__ == "__main__":
         "epochs": 10,
         "mask_percentage": 0.2,
         "kernel_size": 3,
+        "n_cnn_layer": 3,
+        "dilation":1,
         "context_length": 400,
         "batch_size": 100,
         "learning_rate": 0.0001,
