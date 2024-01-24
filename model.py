e98afabfdd33eb058dd1639a207bf0a5ade71c58
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-01-23 18:44:45 -0800

    ...

--- model.py
+++ model.py
@@ -659,7 +659,7 @@ class EpiDenoise20(nn.Module):
             ConvTower(
                 d_model // (2**(n_cnn_layer-i)), 
                 d_model // (2**(n_cnn_layer-i-1)),
-                kernel_size, 1, dilation
+                kernel_size//2, 1, dilation
             ) for i in range(n_cnn_layer)
         ])
 
@@ -673,9 +673,9 @@ class EpiDenoise20(nn.Module):
         self.deconvtower = nn.Sequential(*[
             DeconvBlock(
                 d_model // (2**(i)), d_model // (2**(i+1)), 
-                kernel_size, 2, 1) for i in range(n_cnn_layer - 1)
+                kernel_size//2, 2, 1) for i in range(n_cnn_layer - 1)
         ])
-        self.deconv1 = DeconvBlock(d_model // (2**(n_cnn_layer-1)), d_model // (2**(n_cnn_layer)), kernel_size, 2, 1)
+        self.deconv1 = DeconvBlock(d_model // (2**(n_cnn_layer-1)), d_model // (2**(n_cnn_layer)), kernel_size//2, 2, 1)
         self.deconv2 = DeconvBlock(d_model // (2**(n_cnn_layer)), input_dim, kernel_size, 2, 1)
 
         self.signal_decoder = nn.Linear(input_dim, output_dim)
@@ -2428,8 +2428,8 @@ if __name__ == "__main__":
         "nlayers": 1,
         "epochs": 10,
         "mask_percentage": 0.2,
-        "kernel_size": 3,
-        "n_cnn_layer": 5,
+        "kernel_size": 7,
+        "n_cnn_layer": 3,
         "dilation":1,
         "context_length": 1600,
         "batch_size": 100,
