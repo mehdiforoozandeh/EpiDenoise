30085f62e91d371d1ac0d1f6427a90f4adb814ae
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-01-25 01:00:50 -0800

    ...

--- model.py
+++ model.py
@@ -1846,10 +1846,13 @@ class PRE_TRAINER(object):
                             x_batch = pattern_batch[i:i+batch_size]
                             missing_mask_batch = missing_mask_patten_batch[i:i+batch_size]
                             
-                            # Masking a subset of the input data -- genomic position mask
-                            masked_x_batch, cloze_mask = mask_data16(
-                                x_batch, available_assays_ind, mask_value=-1, mask_percentage=mask_percentage)
-
+                            if len(available_assays_ind) == 1:
+                                masked_x_batch, cloze_mask = mask_data16(
+                                    x_batch, available_assays_ind, mask_value=-1, mask_percentage=mask_percentage)
+                            else:
+                                masked_x_batch, cloze_mask = mask_data18(
+                                    x_batch, available_assays_ind, mask_value=-1, mask_percentage=mask_percentage)
+                                
                             union_mask = cloze_mask | missing_mask_batch
 
                             masked_x_batch = add_noise(masked_x_batch, 0.1)
