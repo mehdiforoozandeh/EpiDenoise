a934b53232c214ea5b16181226b76a4d6dbc0591
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-01-29 10:53:22 -0800

    ...

--- model.py
+++ model.py
@@ -836,8 +836,8 @@ class EpiDenoise21(nn.Module):
         ])
         self.deconvF = DeconvBlock(reversed_channels[-1], output_dim, reversed_kernels[-1], 2, dilation)
 
-        self.signal_decoder =  nn.Linear(d_model, output_dim)
-        self.mask_decoder = nn.Linear(d_model, output_dim)
+        self.signal_decoder =  nn.Linear(output_dim, output_dim)
+        self.mask_decoder = nn.Linear(output_dim, output_dim)
 
     def forward(self, x, m):
         m = m.permute(0, 2, 1) # to N, F, L
