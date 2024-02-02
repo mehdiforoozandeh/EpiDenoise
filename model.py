3424b3465236e71a9dc0044262d9792b25856ce1
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-02-02 13:56:59 -0800

    ...

--- model.py
+++ model.py
@@ -1092,7 +1092,9 @@ class PRE_TRAINER(object):
         
         P = torch.empty_like(X, device="cpu").unsqueeze(0)
         for i in range(start_index, end_index - context_length, step_size):
-
+            print(i)
+            continue
+        
 
             context = X[i:i+context_length, :].unsqueeze(0).to(self.device)
             missing_msk_src = mask[i:i+context_length, :].unsqueeze(0).to(self.device) 
