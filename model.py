8fa62c2aca31c4e61e3cfcf6b64dbf14ee7a7693
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-02-03 18:10:25 -0800

    ...

--- model.py
+++ model.py
@@ -2950,8 +2950,6 @@ def train_epidenoise21(hyper_parameters, checkpoint_path=None, start_ds=0):
         input_dim, conv_out_channels, kernel_size, dilation, nhead, 
         d_model, nlayers, output_dim, dropout=dropout, context_length=context_length)
 
-    model = model.half()
-
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=330, gamma=0.5)
 
