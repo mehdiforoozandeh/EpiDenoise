38d23bf24c9cf6efa86b11a8ba3ee198d4803634
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-02-03 16:47:21 -0800

    ...

--- model.py
+++ model.py
@@ -1158,9 +1158,9 @@ class PRE_TRAINER(object):
         bw_outputs = bw_outputs.reshape(bw_outputs.shape[0]*bw_outputs.shape[1], bw_outputs.shape[2])
         bw_outputs = torch.flip(bw_outputs, dims=(0,))
 
-        del src_context, trg_context, missing_mask, trg_msk, bw_outputs, fw_outputs
-
         P = torch.cat((bw_outputs, fw_outputs), dim=0).cpu()
+
+        del src_context, trg_context, missing_mask, trg_msk, bw_outputs, fw_outputs
 
         mses = []
         spearmans = []
