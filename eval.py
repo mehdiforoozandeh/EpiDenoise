e8498713cad400b650dc0faf3d48bf19daabf281
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-02-20 14:24:09 -0800

    ...

--- eval.py
+++ eval.py
@@ -8,6 +8,191 @@ os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
 PROC_GENE_BED_FPATH = "data/gene_bodies.bed"
 PROC_PROM_BED_PATH = "data/tss.bed"
 
+class METRICS(object):
+    def __init__(self):
+        pass
+
+    def get_gene_positions(self, chrom, bin_size):
+        gene_df = pd.read_csv(PROC_GENE_BED_FPATH, sep='\t', header=None,
+                              names=['chrom', 'start', 'end', 'gene_id', 'gene_name'])
+        chrom_subset = gene_df[gene_df['chrom'] == chrom].copy()
+
+        chrom_subset['start'] = (chrom_subset['start'] / bin_size).apply(lambda s: math.floor(s))
+        chrom_subset['end'] = (chrom_subset['end'] / bin_size).apply(lambda s: math.floor(s))
+        return chrom_subset
+
+    def get_prom_positions(self, chrom, bin_size):
+        prom_df = pd.read_csv(PROC_PROM_BED_PATH, sep='\t', header=None,
+                              names=['chrom', 'start', 'end', 'gene_id', 'gene_name', "strand"])
+        chrom_subset = prom_df[prom_df['chrom'] == chrom].copy()
+
+        chrom_subset['start'] = (chrom_subset['start'] / bin_size).apply(lambda s: math.floor(s))
+        chrom_subset['end'] = (chrom_subset['end'] / bin_size).apply(lambda s: math.floor(s))
+
+        return chrom_subset
+
+    def get_signals(self, array, df):
+        signals = []
+        for idx, row in df.iterrows():
+            gene_bins = slice(row['start'], row['end'])
+            signals += array[gene_bins].tolist()
+
+        return signals
+
+    ################################################################################
+
+    def mse(self, y_true, y_pred):
+        """
+        Calculate the genome-wide Mean Squared Error (MSE). This is a measure of the average squared difference 
+        between the true and predicted values across the entire genome at a resolution of 25bp.
+        """
+        return np.mean((np.array(y_true) - np.array(y_pred))**2)
+
+    def mse_gene(self, y_true, y_pred, chrom='chr21', bin_size=25):
+        assert chrom == 'chr21', f'Got evaluation with unsupported chromosome {chrom}'
+
+        gene_df = self.get_gene_positions(chrom, bin_size)
+        gt_vals = self.get_signals(array=y_true, df=gene_df)
+        pred_vals = self.get_signals(array=y_pred, df=gene_df)
+
+        return self.mse(y_true=gt_vals, y_pred=pred_vals)
+
+    def pearson_gene(self, y_true, y_pred, chrom='chr21', bin_size=25):
+        assert chrom == 'chr21', f'Got evaluation with unsupported chromosome {chrom}'
+
+        gene_df = self.get_gene_positions(chrom, bin_size)
+        gt_vals = self.get_signals(array=y_true, df=gene_df)
+        pred_vals = self.get_signals(array=y_pred, df=gene_df)
+
+        return self.pearson(y_true=gt_vals, y_pred=pred_vals)
+
+    def spearman_gene(self, y_true, y_pred, chrom='chr21', bin_size=25):
+        assert chrom == 'chr21', f'Got evaluation with unsupported chromosome {chrom}'
+
+        gene_df = self.get_gene_positions(chrom, bin_size)
+        gt_vals = self.get_signals(array=y_true, df=gene_df)
+        pred_vals = self.get_signals(array=y_pred, df=gene_df)
+
+        return self.spearman(y_true=gt_vals, y_pred=pred_vals)
+
+    def pearson(self, y_true, y_pred):
+        """
+        Calculate the genome-wide Pearson Correlation. This measures the linear relationship between the true 
+        and predicted values across the entire genome at a resolution of 25bp.
+        """
+        return pearsonr(y_pred, y_true)[0]
+
+    def spearman(self, y_true, y_pred):
+        """
+        Calculate the genome-wide Spearman Correlation. This measures the monotonic relationship between the true 
+        and predicted values across the entire genome at a resolution of 25bp.
+        """
+        return spearmanr(y_pred, y_true)[0]
+
+    def mse_prom(self, y_true, y_pred, chrom='chr21', bin_size=25):
+        assert chrom == 'chr21', f'Got evaluation with unsupported chromosome {chrom}'
+
+        prom_df = self.get_prom_positions(chrom, bin_size)
+        gt_vals = self.get_signals(array=y_true, df=prom_df)
+        pred_vals = self.get_signals(array=y_pred, df=prom_df)
+
+        return self.mse(y_true=gt_vals, y_pred=pred_vals)
+
+    def pearson_prom(self, y_true, y_pred, chrom='chr21', bin_size=25):
+        assert chrom == 'chr21', f'Got evaluation with unsupported chromosome {chrom}'
+
+        prom_df = self.get_prom_positions(chrom, bin_size)
+        gt_vals = self.get_signals(array=y_true, df=prom_df)
+        pred_vals = self.get_signals(array=y_pred, df=prom_df)
+
+        return self.pearson(y_true=gt_vals, y_pred=pred_vals)
+
+    def spearman_prom(self, y_true, y_pred, chrom='chr21', bin_size=25):
+        assert chrom == 'chr21', f'Got evaluation with unsupported chromosome {chrom}'
+
+        prom_df = self.get_prom_positions(chrom, bin_size)
+        gt_vals = self.get_signals(array=y_true, df=prom_df)
+        pred_vals = self.get_signals(array=y_pred, df=prom_df)
+
+        return self.spearman(y_true=gt_vals, y_pred=pred_vals)
+
+    def mse1obs(self, y_true, y_pred):
+        """
+        Calculate the Mean Squared Error at the top 1% of genomic positions ranked by experimental signal (mse1obs). 
+        This is a measure of how well predictions match observations at positions with high experimental signal. 
+        It's similar to recall.
+        """
+        top_1_percent = int(0.01 * len(y_true))
+        top_1_percent_indices = np.argsort(y_true)[-top_1_percent:]
+        return mean_squared_error(y_true[top_1_percent_indices], y_pred[top_1_percent_indices])
+
+    def mse1imp(self, y_true, y_pred):
+        """
+        Calculate the Mean Squared Error at the top 1% of genomic positions ranked by predicted signal (mse1imp). 
+        This is a measure of how well predictions match observations at positions with high predicted signal. 
+        It's similar to precision.
+        """
+        top_1_percent = int(0.01 * len(y_pred))
+        top_1_percent_indices = np.argsort(y_pred)[-top_1_percent:]
+        return mean_squared_error(y_true[top_1_percent_indices], y_pred[top_1_percent_indices])
+
+    def pearson1_obs(self, y_true, y_pred):
+        perc_99 = np.percentile(y_true, 99)
+        perc_99_pos = np.where(y_true >= perc_99)[0]
+
+        return self.pearson(y_true[perc_99_pos], y_pred[perc_99_pos])
+
+    def spearman1_obs(self, y_true, y_pred):
+        perc_99 = np.percentile(y_true, 99)
+        perc_99_pos = np.where(y_true >= perc_99)[0]
+
+        return self.spearman(y_true[perc_99_pos], y_pred[perc_99_pos])
+
+    def pearson1_imp(self, y_true, y_pred):
+        perc_99 = np.percentile(y_pred, 99)
+        perc_99_pos = np.where(y_pred >= perc_99)[0]
+
+        return self.pearson(y_true[perc_99_pos], y_pred[perc_99_pos])
+
+    def spearman1_imp(self, y_true, y_pred):
+        perc_99 = np.percentile(y_pred, 99)
+        perc_99_pos = np.where(y_pred >= perc_99)[0]
+
+        return self.spearman(y_true[perc_99_pos], y_pred[perc_99_pos])
+
+    def peak_overlap(self, y_true, y_pred, p=0.01):
+        if p == 0:
+            return 0
+
+        elif p == 1:
+            return 1
+        
+        top_p_percent = int(p * len(y_true))
+        
+        # Get the indices of the top p percent of the observed (true) values
+        top_p_percent_obs_i = np.argsort(y_true)[-top_p_percent:]
+        
+        # Get the indices of the top p percent of the predicted values
+        top_p_percent_pred_i = np.argsort(y_pred)[-top_p_percent:]
+
+        # Calculate the overlap
+        overlap = len(np.intersect1d(top_p_percent_obs_i, top_p_percent_pred_i))
+
+        # Calculate the percentage of overlap
+        self.overlap_percent = overlap / top_p_percent 
+
+        return self.overlap_percent
+
+    def correspondence_curve(self, y_true, y_pred):
+        curve = []
+        steps = [float(p / 100) for p_i in range(0, 100, 1)]
+        for p in steps:
+            curve.append(
+                (p, peak_overlap(y_true, y_pred, p=p))
+            )
+
+        return curve
+
 class Evaluation: # on chr21
     def __init__(
         self, model_path, hyper_parameters_path, 
@@ -306,7 +491,6 @@ class Evaluation: # on chr21
         P = P.view((P.shape[0] * P.shape[1]), P.shape[-1]) # preds
         torch.save(P, savedir+ bios_name + "_imp.pt")
 
-
     def evaluate_model(self, outdir):
         for bios in self.eval_data.keys():
             print("evaluating ", bios)
@@ -314,6 +498,8 @@ class Evaluation: # on chr21
 
         self.results = pd.DataFrame(self.results)
         self.results.to_csv(outdir, index=False)
+
+    ################################################################################
 
     def get_gene_positions(self, chrom, bin_size):
         gene_df = pd.read_csv(PROC_GENE_BED_FPATH, sep='\t', header=None,
@@ -489,7 +675,271 @@ class Evaluation: # on chr21
         3. sum(abs(log(derivative of correspondence curve))) --> near zero is better 
     """
  
+class EVAL(object): # on chr21
+    def __init__(
+        self, model, traindata_path, evaldata_path, context_length, batch_size,
+        train_log={}, version="18", resolution=25, is_arcsin=True):
+
+        self.traindata_path = traindata_path
+        self.evaldata_path = evaldata_path
+        self.is_arcsin = is_arcsin
+        self.version = version
+        self.context_length = context_length
+        self.batch_size = batch_size
+
+        self.model = model
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.model = self.model.to(self.device)
+        self.model.eval()  # set the model to evaluation mode
+        
+        print(f"# model_parameters: {count_parameters(self.model)}")
+
+        self.all_assays = ['M{:02d}'.format(i) for i in range(1, 36)]
+
+        main_chrs = ["chr" + str(x) for x in range(1,23)] + ["chrX"]
+        self.chr_sizes = {}
+        self.resolution = resolution
+
+        with open(chr_sizes_file, 'r') as f:
+            for line in f:
+                chr_name, chr_size = line.strip().split('\t')
+                if chr_name in main_chrs:
+                    self.chr_sizes[chr_name] = int(chr_size)
+
+        self.results = []
+
+        self.train_data = {}
+        self.eval_data = {}
+
+        self.train_data = {}
+        self.eval_data = {}
+        self.metrics = METRICS()
+
+        # load and bin chr21 of all bigwig files 
+        for t in os.listdir(traindata_path):
+            if ".bigwig" in t:
+
+                for e in os.listdir(evaldata_path):
+                    if ".bigwig" in e:
+                        
+                        if t[:3] == e[:3]:
+
+                            if t[:3] not in self.train_data:
+                                self.train_data[t[:3]] = {}
+
+                            if e[:3] not in self.eval_data:
+                                self.eval_data[e[:3]] = {}
+
+                            self.train_data[t[:3]][t[3:6]] = traindata_path + "/" + t
+                            self.eval_data[e[:3]][e[3:6]] = evaldata_path + "/" + e
+
+        print(self.eval_data.keys())
+        print(self.train_data.keys())
+
+    def load_tensor(self, bios_name, mode="train"):
+        chr, start, end = "chr21", 0, self.chr_sizes["chr21"]
+        all_samples = []
+        missing_ind = []
+
+        if mode  == "train": 
+            source = self.train_data
+            savepath = self.traindata_path + f"/{bios_name}_chr21_{self.resolution}.pt"
+        elif mode == "eval":
+            source = self.eval_data
+            savepath = self.evaldata_path + f"/{bios_name}_chr21_{self.resolution}.pt"
+        
+        if os.path.exists(savepath):
+            all_samples = torch.load(savepath)
+            # fill-in missing_ind
+            for i in range(all_samples.shape[1]):
+                if (all_samples[:, i] == -1).all():
+                    missing_ind.append(i)
+                    
+            return all_samples, missing_ind
+
+        else:
+            for i in range(len(self.all_assays)):
+                assay = self.all_assays[i]
+                if assay in source[bios_name].keys():
+                    print("loading ", assay)
+                    bw = pyBigWig.open(source[bios_name][assay])
+                    signals = bw.stats(chr, start, end, type="mean", nBins=(end - start) // self.resolution)
+                
+                else:
+                    print(assay, "missing")
+                    signals = [-1 for _ in range((end - start) // self.resolution)]
+                    missing_ind.append(i)
+
+                all_samples.append(signals)
+
+            all_samples = torch.from_numpy(np.array(all_samples, dtype=np.float32)).transpose_(0, 1)
+
+            # replace NaN with zero
+            all_samples = torch.where(torch.isnan(all_samples), torch.zeros_like(all_samples), all_samples)
+
+            nan_count = torch.isnan(all_samples).sum().item()
+            minus_one_count = (all_samples == -1).sum().item()
+
+            torch.save(all_samples, savepath)
+            
+            return all_samples, missing_ind
+
+    def load_bios(self, bios_name):
+        X, missing_x_i = self.load_tensor(bios_name, mode="train")
+        Y, missing_y_i = self.load_tensor(bios_name, mode="eval")
+
+        num_rows = (X.shape[0] // self.context_length) * self.context_length
+        X, Y = X[:num_rows, :], Y[:num_rows, :]
+
+        if self.is_arcsin:
+            arcmask1 = (X != -1)
+            X[arcmask1] = torch.arcsinh_(X[arcmask1])
+
+            arcmask2 = (Y != -1)
+            Y[arcmask2] = torch.arcsinh_(Y[arcmask2])
+
+        X = X.view(-1, self.context_length, X.shape[-1])
+        Y = Y.view(-1, self.context_length, Y.shape[-1])
+        
+        return X, Y, missing_x_i, missing_y_i
+
+    def get_imp(self, X, missing_x_i): # X: train data
+        d_model = X.shape[-1]
+
+        # Initialize a tensor to store all predictions
+        P = torch.empty_like(X, device="cpu")
+
+        # make predictions in batches
+        for i in range(0, len(X), self.batch_size):
+            torch.cuda.empty_cache()
+            
+            x_batch = X[i:i+ self.batch_size]
+
+            with torch.no_grad():
+                x_batch = x_batch.to(self.device)
+
+                if self.version == "18":
+                    outputs, pred_mask = self.model(x_batch)
+
+                elif self.version in ["20", "21"]:
+                    mask = torch.zeros_like(x_batch, dtype=torch.bool, device=self.device)
+                    for ii in missing_x_i: 
+                        mask[:,:,ii] = True
+
+                    mask = mask.to(self.device)
+                    outputs, pred_mask = self.model(x_batch, mask)
+
+            # Store the predictions in the large tensor
+            P[i:i+outputs.shape[0], :, :] = outputs.cpu()
+        
+        return P
+
+    def get_metrics(self, X, Y, P, missing_x_i, missing_y_i):
+        results = []
+        for j in range(Y.shape[-1]):  # for each feature i.e. assay
+            pred = P[:, j].numpy()
+            metrics_list = []
+
+            if j in missing_x_i and j not in missing_y_i:  # if the feature is missing in the input
+                target = Y[:, j].numpy()
+                comparison = 'imputed'
+            
+            elif j not in missing_x_i:
+                target = X[:, j].numpy()
+                comparison = 'denoised'
+
+            else:
+                continue
+            
+            metrics = {
+                'assay': self.all_assays[j],
+                'comparison': comparison,
+                'available train assays': len(self.all_assays) - len(missing_x_i),
+                'available eval assays': len(self.all_assays) - len(missing_y_i),
+
+                "obs":target,
+                "imp":pred,
+
+                'MSE-GW': self.metrics.mse(target, pred),
+                'Pearson-GW': self.metrics.pearson(target, pred),
+                'Spearman-GW': self.metrics.spearman(target, pred),
+
+                'MSE-1obs': self.metrics.mse1obs(target, pred),
+                'Pearson_1obs': self.metrics.pearson1_obs(target, pred),
+                'Spearman_1obs': self.metrics.spearman1_obs(target, pred),
+
+                'MSE-1imp': self.metrics.mse1imp(target, pred),
+                'Pearson_1imp': self.metrics.pearson1_imp(target, pred),
+                'Spearman_1imp': self.metrics.spearman1_imp(target, pred),
+
+                'MSE-gene': self.metrics.mse_gene(target, pred),
+                'Pearson_gene': self.metrics.pearson_gene(target, pred),
+                'Spearman_gene': self.metrics.spearman_gene(target, pred),
+
+                'MSE-prom': self.metrics.mse_prom(target, pred),
+                'Pearson_prom': self.metrics.pearson_prom(target, pred),
+                'Spearman_prom': self.metrics.spearman_prom(target, pred),
+
+                "peak_overlap_01thr": self.metrics.peak_overlap(target, pred, p=0.01),
+                "peak_overlap_05thr": self.metrics.peak_overlap(target, pred, p=0.05),
+                "peak_overlap_10thr": self.metrics.peak_overlap(target, pred, p=0.10),
+
+                "corresp_curve": self.metrics.correspondence_curve(target, pred)
+            }
+            results.append(metrics)
+        
+        return results
+
+    def bios_pipeline(self, bios_name, test=False):
+        """
+        load bios
+        get imp
+        get metrics
+        """
+        if test:
+            X = torch.load("data/C23_trn.pt")
+            # fill-in missing_ind
+            for i in range(X.shape[1]):
+                if (X[:, i] == -1).all():
+                    missing_x_ind.append(i)
+            
+            Y = torch.load("data/C23_val.pt")
+            # fill-in missing_ind
+            for i in range(Y.shape[1]):
+                if (Y[:, i] == -1).all():
+                    missing_y_ind.append(i)
+
+            P = torch.load("data/C23_imp.pt")
+
+            print(X.shape)
+            print(Y.shape)
+            print(P.shape)
+
+        else:
+            X, Y, missing_x_i, missing_y_i = self.load_bios(bios_name)
+            P = self.get_imp(X, missing_x_i)
+
+            P = P.view((P.shape[0] * P.shape[1]), P.shape[-1]) # preds
+            Y = Y.view((Y.shape[0] * Y.shape[1]), Y.shape[-1]) # eval data
+            X = X.view((X.shape[0] * X.shape[1]), X.shape[-1]) # train data
+
+        eval_res = self.get_metrics(X, Y, P, missing_x_i, missing_y_i)
+        return eval_res
+
+    def viz(self):
+        pass
+
 if __name__=="__main__":
+    e = EVAL(
+        model= "models/EPD18_model_checkpoint_ds_10.pth", 
+        hyper_parameters_path= "models/hyper_parameters18_EpiDenoise18_20240220112009_params170630.pkl", 
+        traindata_path="/project/compbio-lab/EIC/training_data/", 
+        evaldata_path="/project/compbio-lab/EIC/validation_data/", 
+        context_length=200, batch_size=200)
+
+    e.bios_pipeline("C23")
+
+    exit()
     e = Evaluation(
         model_path= "models/EPD18_model_checkpoint_ds_10.pth", 
         hyper_parameters_path= "models/hyper_parameters18_EpiDenoise18_20240220112009_params170630.pkl", 
