b0245e7d85837bf8053dd30b85bb028efa74c5b5
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-02-23 20:07:36 -0800

    ...

--- model.py
+++ model.py
@@ -2654,7 +2654,7 @@ class PRE_TRAINER(object):
 
                             print(
                                 outputs[cloze_mask].min().item(), outputs[cloze_mask].max().item(),
-                                x_batch[cloze_mask].min().item(), x_batch[cloze_mask].max().item()
+                                x_batch[cloze_mask].min().item(), x_batch[cloze_mask].mean().item()
                             )
 
                             if torch.isnan(loss).sum() > 0:
