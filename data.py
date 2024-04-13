c9ff87f3ae0aded06dba2fb0de1ab4753683e0aa
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-04-13 12:15:20 -0700

    ..

--- data.py
+++ data.py
@@ -955,7 +955,8 @@ class ExtendedEncodeDataHandler:
         missing_exp = self.is_bios_complete(bios_name)
         for exp in missing_exp:
             rows = self.df3.loc[(self.df3["bios"] == bios_name)&(self.df3["exp"] == exp), :]
-            print(rows.reset_index(drop=True))
+            print(rows)
+            # print(rows.reset_index(drop=True))
 
     def set_alias(self):
         """Set aliases for biosamples, experiments, and donors based on data availability."""
@@ -1078,6 +1079,7 @@ if __name__ == "__main__":
     if sys.argv[1] == "check":
         eed = ExtendedEncodeDataHandler(solar_data_path)
         print(eed.is_bios_complete(sys.argv[2]))
+
     elif sys.argv[1] == "fix":
         eed = ExtendedEncodeDataHandler(solar_data_path)
         eed.fix_bios(sys.argv[2])
