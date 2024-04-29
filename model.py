2cb5f8ac20da347520643dbba7cee1e4fb846939
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-04-28 19:47:59 -0700

    ...

--- model.py
+++ model.py
@@ -4254,7 +4254,7 @@ if __name__ == "__main__":
             "mask_percentage": 0.25,
             "context_length": 400,
             "batch_size": 30,
-            "learning_rate": 2e-4,
+            "learning_rate": 1e-2,
             "num_loci": 200,
             "lr_halflife":1,
             "min_avail":5
