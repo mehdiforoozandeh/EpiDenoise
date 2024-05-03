e5b43d6a7354da11c46341e268e1345ded754d67
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-05-03 13:52:38 -0700

    ...

--- model.py
+++ model.py
@@ -4294,7 +4294,7 @@ if __name__ == "__main__":
             "inner_epochs": 10,
             "mask_percentage": 0.25,
             "context_length": 400,
-            "batch_size": 100,
+            "batch_size": 50,
             "learning_rate": 5e-4,
             "num_loci": 500,
             "lr_halflife":1,
