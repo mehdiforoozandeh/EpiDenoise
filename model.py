f29d407e68c9417ef4b37942368861fa10f1555d
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-05-08 10:25:13 -0700

    ...

--- model.py
+++ model.py
@@ -1386,8 +1386,8 @@ class EpiDenoise30a(nn.Module):
 
         p, n = self.neg_binom_layer(src)
         
-        p = p.view(b, l, src.shape[1])
-        n = n.view(b, l, src.shape[1])
+        p = p.view(b, l, p.shape[1])
+        n = n.view(b, l, n.shape[1])
 
         print(n.shape, p.shape)
         exit()
