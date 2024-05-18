1f513533e443f94cfb35482db8aa883c11474f50
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-05-17 20:08:13 -0700

    ..

--- model.py
+++ model.py
@@ -838,8 +838,11 @@ class ComboLoss_NBNLL(nn.Module):
         self.reduction = 'sum'
 
     def forward(self, p_pred, n_pred, true_signals, masked_map, obs_map):
-        obs_map = obs_map[0,:,:]
-        masked_map = masked_map[0,:,:]
+        obs_map = obs_map.clone()
+        obs_map[:,:,:] = False
+        obs_map[0,:,:] = True
+        masked_map = obs_map
+
         ups_y_true, ups_n_pred, ups_p_pred = true_signals[obs_map], n_pred[obs_map], p_pred[obs_map]
         imp_y_true, imp_n_pred, imp_p_pred = true_signals[masked_map], n_pred[masked_map], p_pred[masked_map]
 
