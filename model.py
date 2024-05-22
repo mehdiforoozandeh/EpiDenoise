3f1689d3f8ca7926afb6382828aa318dffed702f
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-05-22 13:21:40 -0700

    ...

--- model.py
+++ model.py
@@ -11,8 +11,6 @@ from sklearn.metrics import r2_score
 from datetime import datetime
 from scipy.stats import nbinom
 
-from evaluate import MONITOR_VALIDATION
-
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:256"
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 
