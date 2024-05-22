7b0b9b20a492ab0e4347f1fde27d0a73a2781920
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-05-22 13:16:50 -0700

    ...

--- eval.py
+++ eval.py
@@ -1,4 +1,4 @@
-from model import *
+from model import MODEL_LOADER
 from data import *
 from scipy.stats import pearsonr, spearmanr, poisson, rankdata
 from sklearn.metrics import mean_squared_error, r2_score
