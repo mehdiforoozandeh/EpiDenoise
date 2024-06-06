d1fc019a9007c0b1dad353cd954b8288e31b73e5
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-06-05 19:40:30 -0700

    ...

--- model.py
+++ model.py
@@ -1684,25 +1684,16 @@ class EpiDenoise30c(nn.Module):
         for conv in self.convD:
             H = conv(H)
 
-        print(H.shape)
+        # H.shape =  N, F', L'
+
         # Aggregating the sequence representation
         H_avg_pool = F.adaptive_avg_pool1d(H, 1).squeeze(-1)  # Global Average Pooling
         H_max_pool = F.adaptive_max_pool1d(H, 1).squeeze(-1)  # Global Max Pooling
 
-        print(H_avg_pool.shape)
-        print(H_max_pool.shape)
-
-        # H = torch.cat((H_avg_pool, H_max_pool), dim=-1)  # Concatenate pooled features
-        H = self.alpha * H_avg_pool + self.beta * H_max_pool  # Shape: (batch_size, feature_dim)
-        # print(H.shape)
-        # H = self.fusionD(H)
-        print(H.shape)
+        H = (self.alpha * H_avg_pool) + (self.beta * H_max_pool)  # Shape: (batch_size, feature_dim)
 
         # Transforming the aggregated representation
         H = H.unsqueeze(-1).expand(-1, -1, self.l2)
-
-        print(H.shape)
-        exit()
 
         # H.shape =  N, F', L'
         for encD in self.transD:
