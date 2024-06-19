5f1dd27385ad5dd2d34e5b5e70e459a0f1a91924
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-06-19 13:18:16 -0700

    ...

--- model.py
+++ model.py
@@ -53,8 +53,7 @@ class MetadataEmbeddingModule(nn.Module):
         runtype = torch.where(runtype == -1, torch.tensor(2, device=runtype.device), runtype) # missing
         runtype = torch.where(runtype == -2, torch.tensor(3, device=runtype.device), runtype) # cloze_masked
 
-        print(torch.unique(runtype))
-        exit()
+        print(side, torch.unique(runtype))
 
         if side == "x":
             depth_embed = self.xdepth_transform(depth)
