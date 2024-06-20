1f2104ee161e9efd50eef3da4cab600d937020fa
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-06-20 15:12:01 -0700

    ...

--- data.py
+++ data.py
@@ -1567,7 +1567,7 @@ class SyntheticData:
 
         return transformed_sequences
 
-    def smooth_sequence(self, sequence, sigma=10):
+    def smooth_sequence(self, sequence, sigma=1):
         return gaussian_filter1d(sequence, sigma=sigma)
 
     def apply_smoothing(self, sequences):
