55d161eaa69576ff887ebdf674fb2901b3a7afa3
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-06-21 01:19:25 -0700

    ...

--- model.py
+++ model.py
@@ -3536,7 +3536,7 @@ class PRE_TRAINER(object):
                 # print("new batch")
                 # Randomly choose two downsampling factors and assign them to dsf_X and dsf_Y based on their values
                 dsf_X, dsf_Y = sorted(random.choices(dsf_list, k=2), reverse=True) # dsf_X is of equal or higher dsf
-                dsf_Y = 1
+                dsf_X, dsf_Y = 1
 
                 _X_batch, _mX_batch, _avX_batch = self.dataset.get_batch(dsf_X)
                 _Y_batch, _mY_batch, _avY_batch = self.dataset.get_batch(dsf_Y)
@@ -5026,12 +5026,12 @@ if __name__ == "__main__":
             "d_model": 768,
             "nlayers": 6,
             "epochs": 1,
-            "inner_epochs": 50,
+            "inner_epochs": 10,
             "mask_percentage": 0.1,
             "context_length": 810,
             "batch_size": 25,
             "learning_rate": 1e-4,
-            "num_loci": 800,
+            "num_loci": 200,
             "lr_halflife":2,
             "min_avail":8
         }
