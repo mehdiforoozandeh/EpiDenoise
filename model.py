cfe76f4242a8bb104fe9977c753053a736ec69c9
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-06-28 14:35:32 -0700

    ...

--- model.py
+++ model.py
@@ -3904,7 +3904,7 @@ class PRE_TRAINER(object):
                         loss = (mask_percentage * obs_loss) + (pred_loss * (1 - mask_percentage)) + msk_p_loss + msk_o_loss
                         # loss = pred_loss #+ msk_p_loss + msk_o_loss
 
-                    elif arch in ["c"..]:
+                    elif arch in ["c"]:
                         output_p, output_n = self.model(X_batch, mX_batch, mY_batch, avX_batch)
                         obs_loss = self.criterion(output_p, output_n, Y_batch, observed_map) 
                         loss = obs_loss
