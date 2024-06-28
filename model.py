f6e6da3798c3dd1224980ec8049aeb0e6b70d06f
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-06-28 14:01:19 -0700

    ...

--- model.py
+++ model.py
@@ -1577,6 +1577,9 @@ class EpiDenoise30b(nn.Module):
         self.transformer_encoder = nn.ModuleList(
             [self.encoder_layer for _ in range(nlayers)])
 
+        print([reverse_conv_channels[i + 1] for i in range(n_cnn_layers) if i + 1 < n_cnn_layers else reverse_conv_channels[i] / 2])
+        print([reverse_conv_channels[i] for i in range(n_cnn_layers)])
+        exit()
         self.deconv_layers = nn.ModuleList(
             [DeconvTower(
                 reverse_conv_channels[i], reverse_conv_channels[i + 1] if i + 1 < n_cnn_layers else reverse_conv_channels[i] / 2,
