72284a4a95bf234efa5846fed201572d4d840f7b
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-06-29 23:22:31 -0700

    ...

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ from _utils import *
 from sklearn.metrics import r2_score
 from datetime import datetime
 from scipy.stats import nbinom
-import imageio
+import imageio.v2 as imageio
 from io import BytesIO
 
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:256"
