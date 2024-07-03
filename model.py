8ade4961e13b2445e3ef61e93f96d574e81ea626
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-07-03 12:49:23 -0700

    ...

--- model.py
+++ model.py
@@ -5039,7 +5039,7 @@ def train_epidenoise30(hyper_parameters, checkpoint_path=None, arch="a"):
         n_cnn_layers, nhead, d_model, nlayers, output_dim, pool_size = pool_size,
         dropout=dropout, context_length=context_length, pos_enc="relative")
 
-    optimizer = optim.Adamax(model.parameters(), lr=learning_rate)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=lr_halflife, gamma=0.5)
     # scheduler = None
@@ -5478,7 +5478,7 @@ if __name__ == "__main__":
             "mask_percentage": 0.2,
             "context_length": 800,
             "batch_size": 50,
-            "learning_rate": 1e-2,
+            "learning_rate": 1e-4,
             "num_loci": 1600,
             "lr_halflife":1,
             "min_avail":10
