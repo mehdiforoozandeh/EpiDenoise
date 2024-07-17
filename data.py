b0feda08219c92070b38b35db181ecf36325715d
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-07-17 13:35:35 -0700

    ...

--- data.py
+++ data.py
@@ -1433,6 +1433,9 @@ class ExtendedEncodeDataHandler:
                         exp_path = os.path.join(self.base_path, bios_name, exp)
                         if os.path.isdir(exp_path):
                             exps.append(exp)
+                            print(exp)
+
+            print(exps)
                 
         else:
             exps = list(self.navigation[bios_name].keys())
