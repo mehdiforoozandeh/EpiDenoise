596b2c7ff7680515f2ffac6f1083cbb892453e9a
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-08-01 00:38:04 -0700

    ...

--- data.py
+++ data.py
@@ -2465,6 +2465,7 @@ if __name__ == "__main__":
                     todo.append([bs, exp])
     
         # multiprocess all bios_name, exp pairs in todo for function eed.get_signal_pval_bigwig(bios_name, exp)
+        print(f"num cpus: {mp.cpu_count()}")
         with mp.Pool(processes=mp.cpu_count()) as pool:
             pool.map(process_pair, todo)
 
