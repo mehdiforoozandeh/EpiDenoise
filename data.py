9e5059ea199fda3913843995b131d525a2bee533
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-08-06 13:42:19 -0700

    ...

--- data.py
+++ data.py
@@ -2040,13 +2040,13 @@ class ExtendedEncodeDataHandler:
             self.loaded_data.append(d)
             self.loaded_metadata.append(md)
 
-        self.Y_loaded_data = self.loaded_data
-        self.Y_loaded_metadata = self.loaded_metadata
+        # self.Y_loaded_data = self.loaded_data
+        # self.Y_loaded_metadata = self.loaded_metadata
 
-        self.Y_loaded_pval = []
-        for bios in batch_bios_list:
-            pval_d = self.load_bios_BW(bios, [list(self.loci.keys())[self.chr_pointer]], self.dsf_list[self.dsf_pointer])
-            self.Y_loaded_pval.append(pval_d)
+        # self.Y_loaded_pval = []
+        # for bios in batch_bios_list:
+        #     pval_d = self.load_bios_BW(bios, [list(self.loci.keys())[self.chr_pointer]], self.dsf_list[self.dsf_pointer])
+        #     self.Y_loaded_pval.append(pval_d)
 
     def update_batch_pointers(self):
         if self.chr_loci_pointer + self.loci_batchsize >= len(self.loci[list(self.loci.keys())[self.chr_pointer]]):
@@ -2084,10 +2084,10 @@ class ExtendedEncodeDataHandler:
                 self.Y_loaded_data = self.loaded_data
                 self.Y_loaded_metadata = self.loaded_metadata
 
-                self.Y_loaded_pval = []
-                for bios in batch_bios_list:
-                    pval_d = self.load_bios_BW(bios, [list(self.loci.keys())[self.chr_pointer]], self.dsf_list[self.dsf_pointer])
-                    self.Y_loaded_pval.append(pval_d)
+                # self.Y_loaded_pval = []
+                # for bios in batch_bios_list:
+                #     pval_d = self.load_bios_BW(bios, [list(self.loci.keys())[self.chr_pointer]], self.dsf_list[self.dsf_pointer])
+                #     self.Y_loaded_pval.append(pval_d)
 
         else:
             self.chr_loci_pointer += self.loci_batchsize
