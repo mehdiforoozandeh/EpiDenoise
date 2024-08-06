e61027e7de102f3185fab142e605ebf467a0a831
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-08-06 13:59:10 -0700

    ...

--- data.py
+++ data.py
@@ -2432,7 +2432,7 @@ if __name__ == "__main__":
         dataset = ExtendedEncodeDataHandler(solar_data_path)
         dataset.initialize_EED(
             m=10, context_length=800*25, 
-            bios_batchsize=1, loci_batchsize=1, loci_gen="random",
+            bios_batchsize=5, loci_batchsize=1, loci_gen="random",
             bios_min_exp_avail_threshold=10, check_completeness=True, eic=True)
 
         for epoch in range(10):
