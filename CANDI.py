ee96b4f4e4bfb200e923ddbacbd4559e82fdf8cf
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-08-06 17:32:56 -0700

    ...

--- CANDI.py
+++ CANDI.py
@@ -114,7 +114,7 @@ class CANDI_DNA():
 
 
 class CANDI_NLL_LOSS(nn.Module):
-    def __init__(self, reduction='sum'):
+    def __init__(self, reduction='mean'):
         super(CANDI_NLL_LOSS, self).__init__()
         self.reduction = reduction
         self.gaus_nll = nn.GaussianNLLLoss(reduction=self.reduction)
