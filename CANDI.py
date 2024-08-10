da80bd61f514d92321acacef9ef40866399a4a13
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-08-10 02:41:01 -0700

    ...

--- CANDI.py
+++ CANDI.py
@@ -288,11 +288,11 @@ class CANDI_DNA(nn.Module):
         return p, n, mu, var
 
 class CANDI_NLL_LOSS(nn.Module):
-    def __init__(self, reduction='sum'):
+    def __init__(self, reduction='mean'):
         super(CANDI_NLL_LOSS, self).__init__()
         self.reduction = reduction
         # self.gaus_nll = nn.GaussianNLLLoss(reduction=self.reduction)
-        self.mse = nn.MSELoss(reduction="sum")
+        self.mse = nn.MSELoss(reduction=reduction)
         self.nbin_nll = negative_binomial_loss
 
     def forward(self, p_pred, n_pred, mu_pred, var_pred, true_count, true_pval, obs_map, masked_map):
