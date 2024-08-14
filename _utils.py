5f2498ea6a072b868b7c48b7a17bba6bcdcf98bc
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-08-13 22:27:09 -0700

    ...

--- _utils.py
+++ _utils.py
@@ -558,6 +558,11 @@ class MONITOR_VALIDATION(object): # CANDI
 
         if self.DNA:
             seq = torch.cat(subsets_seq, dim=0)
+            print(seq.shape)
+            print(X.shape)
+            print(Y.shape)
+            print(P.shape)
+            exit()
 
         X = X.view(-1, self.context_length, X.shape[-1])
         Y = Y.view(-1, self.context_length, Y.shape[-1])
