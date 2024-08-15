b19708c436c5bb393c3f62cb72dfa8f3e48e3428
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-08-15 01:18:08 -0700

    ...

--- CANDI.py
+++ CANDI.py
@@ -730,7 +730,7 @@ def Train_CANDI(hyper_parameters, eic=False, checkpoint_path=None, DNA=False, su
     dataset = ExtendedEncodeDataHandler(data_path)
     dataset.initialize_EED(
         m=num_training_loci, context_length=context_length*resolution, 
-        bios_batchsize=batch_size, loci_batchsize=1, loci_gen="ccre",#["chr19", "chr20"], 
+        bios_batchsize=batch_size, loci_batchsize=1, loci_gen="random",#["chr19", "chr20"], 
         bios_min_exp_avail_threshold=min_avail, check_completeness=True, eic=eic)
 
     signal_dim = dataset.signal_dim
@@ -839,7 +839,7 @@ if __name__ == "__main__":
         "context_length": 1600,
         "batch_size": 50,
         "learning_rate": 5e-4,
-        "num_loci": 3200,
+        "num_loci": 100,
         "lr_halflife":1,
         "min_avail":5}
 
