cd6fa7b0d8cfa4680e088748af0eaae058eb684a
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-08-22 11:59:35 -0700

    ...

--- eval.py
+++ eval.py
@@ -3499,10 +3499,12 @@ class EVAL_CANDI(object):
 
         self.model = model
         self.dataset = ExtendedEncodeDataHandler(self.data_path, resolution=self.resolution)
-        print(self.dataset.signal_dim)
-        exit()
+        
         self.dataset.init_eval(
             self.context_length, check_completeness=True, split=split, bios_min_exp_avail_threshold=5, eic=eic)
+        
+        print(self.dataset.signal_dim)
+        exit()
 
         self.mark_dict = {v: k for k, v in self.dataset.aliases["experiment_aliases"].items()}
 
