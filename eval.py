239c93d4a5832f0aa4c45c73c34fdd36431a40f6
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-08-22 13:25:16 -0700

    ...

--- eval.py
+++ eval.py
@@ -3525,7 +3525,7 @@ class EVAL_CANDI(object):
         self.train_data = {}
         self.eval_data = {}
         self.metrics = METRICS()
-        self.viz = VISUALS(resolution=self.resolution, savedir=self.savedir)
+        self.viz = VISUALS_CANDI(resolution=self.resolution, savedir=self.savedir)
 
         self.gene_coords = load_gene_coords("data/parsed_genecode_data_hg38_release42.csv")
         self.gene_coords = self.gene_coords[self.gene_coords["chr"] == "chr21"].reset_index(drop=True)
@@ -4237,24 +4237,24 @@ class EVAL_CANDI(object):
     def viz_bios(self, eval_res):
         # Define a dictionary mapping function names to corresponding methods
         plot_functions = {
-            "count_track": self.count_track,
-            "signal_track": self.signal_track,
-            "count_confidence": self.count_confidence,
-            "signal_confidence": self.signal_confidence,
-            "quantile_hist": self.quantile_hist,
-            "quantile_heatmap": self.quantile_heatmap,
-            "count_error_std_hexbin": self.count_error_std_hexbin,
-            "signal_error_std_hexbin": self.signal_error_std_hexbin,
-            "count_mean_std_hexbin": self.count_mean_std_hexbin,
-            "signal_mean_std_hexbin": self.signal_mean_std_hexbin,
-            "count_scatter_with_marginals": self.count_scatter_with_marginals,
-            "signal_scatter_with_marginals": self.signal_scatter_with_marginals,
-            "count_heatmap": self.count_heatmap,
-            "signal_heatmap": self.signal_heatmap,
-            "count_rank_heatmap": self.count_rank_heatmap,
-            "signal_rank_heatmap": self.signal_rank_heatmap,
-            "count_context_length_specific_performance": self.count_context_length_specific_performance,
-            "signal_context_length_specific_performance": self.signal_context_length_specific_performance
+            "count_track": self.viz.count_track,
+            "signal_track": self.viz.signal_track,
+            "count_confidence": self.viz.count_confidence,
+            "signal_confidence": self.viz.signal_confidence,
+            "quantile_hist": self.viz.quantile_hist,
+            "quantile_heatmap": self.viz.quantile_heatmap,
+            "count_error_std_hexbin": self.viz.count_error_std_hexbin,
+            "signal_error_std_hexbin": self.viz.signal_error_std_hexbin,
+            "count_mean_std_hexbin": self.viz.count_mean_std_hexbin,
+            "signal_mean_std_hexbin": self.viz.signal_mean_std_hexbin,
+            "count_scatter_with_marginals": self.viz.count_scatter_with_marginals,
+            "signal_scatter_with_marginals": self.viz.signal_scatter_with_marginals,
+            "count_heatmap": self.viz.count_heatmap,
+            "signal_heatmap": self.viz.signal_heatmap,
+            "count_rank_heatmap": self.viz.count_rank_heatmap,
+            "signal_rank_heatmap": self.viz.signal_rank_heatmap,
+            "count_context_length_specific_performance": self.viz.count_context_length_specific_performance,
+            "signal_context_length_specific_performance": self.viz.signal_context_length_specific_performance
         }
         
         for func_name, func in plot_functions.items():
