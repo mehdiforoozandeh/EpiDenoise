b15b033f7ab153a585094f77cdee0a691b089a5e
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-08-30 11:18:37 -0700

    ...

--- CANDI.py
+++ CANDI.py
@@ -953,16 +953,16 @@ if __name__ == "__main__":
         "data_path": "/project/compbio-lab/encode_data/",
         "dropout": 0.1,
 
-        "n_cnn_layers": 5,
+        "n_cnn_layers": 4,
         "conv_kernel_size" : 5,
         "pool_size": 2,
 
-        "nhead": 16,
+        "nhead": 8,
         "n_sab_layers": 8,
-        "epochs": 10,
+        "epochs": 20,
         "inner_epochs": 1,
         "mask_percentage": 0.2,
-        "context_length": 1600,
+        "context_length": 800,
         "batch_size": 50,
         "learning_rate": 1e-3,
         "num_loci": 100,
@@ -1002,4 +1002,4 @@ if __name__ == "__main__":
     if "prog_mask" in sys.argv:
         prg = True
 
-    Train_CANDI(hyper_parameters_L, eic=eic, DNA=DNA, suffix="Aug29-relpos", prog_mask=prg)
+    Train_CANDI(hyper_parameters_L, eic=eic, DNA=DNA, suffix="Aug30-relpos", prog_mask=prg)
