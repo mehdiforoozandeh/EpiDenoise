3654cc23e8c19d2339ed008bfc7582e626a95b68
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-10-04 20:44:29 +0200

    ...

--- CANDI.py
+++ CANDI.py
@@ -1014,14 +1014,14 @@ if __name__ == "__main__":
         "n_cnn_layers": 3,
         "conv_kernel_size" : 5,
         "pool_size": 2,
-        "expansion_factor":3,
+        "expansion_factor":2,
 
         "nhead": 9,
         "n_sab_layers": 4,
         "epochs": 20,
         "inner_epochs": 1,
         "mask_percentage": 0.2, # not used
-        "context_length": 400,
+        "context_length": 800,
         "batch_size": 50,
         "learning_rate": 1e-3,
         "num_loci": 3750,
