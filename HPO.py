5169ba446d2fcc6d2b27dd344c0101cb63c9c6a0
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-10-11 01:57:56 +0200

    ...

--- HPO.py
+++ HPO.py
@@ -120,37 +120,37 @@ if __name__ == "__main__":
         "eic":True,
     }
     hyperparameter_space = [
-        {"n_cnn_layers": 3, "conv_kernel_size": 9, "expansion_factor": 2, 
-            "nhead": 4, "n_sab_layers": 8, "context_length": 1600, "pos_enc": "relative", 
-            "batch_size": 50, "learning_rate": 1e-3, "dna":True},
+        # {"n_cnn_layers": 3, "conv_kernel_size": 9, "expansion_factor": 2, 
+        #     "nhead": 4, "n_sab_layers": 8, "context_length": 1600, "pos_enc": "relative", 
+        #     "batch_size": 50, "learning_rate": 1e-3, "dna":True},
 
-        {"n_cnn_layers": 4, "conv_kernel_size": 5, "expansion_factor": 2, 
-            "nhead": 8, "n_sab_layers": 4, "context_length": 1200, "pos_enc": "relative", 
-            "batch_size": 50, "learning_rate": 1e-3, "dna":True},
+        # {"n_cnn_layers": 4, "conv_kernel_size": 5, "expansion_factor": 2, 
+        #     "nhead": 8, "n_sab_layers": 4, "context_length": 1200, "pos_enc": "relative", 
+        #     "batch_size": 50, "learning_rate": 1e-3, "dna":True},
+
+        # {"n_cnn_layers": 5, "conv_kernel_size": 3, "expansion_factor": 2, 
+        #     "nhead": 16, "n_sab_layers": 2, "context_length": 800, "pos_enc": "relative", 
+        #     "batch_size": 50, "learning_rate": 1e-3, "dna":True},
 
         {"n_cnn_layers": 5, "conv_kernel_size": 3, "expansion_factor": 2, 
-            "nhead": 16, "n_sab_layers": 2, "context_length": 800, "pos_enc": "relative", 
-            "batch_size": 50, "learning_rate": 1e-3, "dna":True},
-
-        {"n_cnn_layers": 3, "conv_kernel_size": 3, "expansion_factor": 2, 
             "nhead": 8, "n_sab_layers": 8, "context_length": 1600, "pos_enc": "relative", 
             "batch_size": 50, "learning_rate": 1e-3, "dna":True},
-            
+
         ################################################################################
 
-        {"n_cnn_layers": 3, "conv_kernel_size": 9, "expansion_factor": 2, 
-            "nhead": 4, "n_sab_layers": 8, "context_length": 1600, "pos_enc": "relative", 
-            "batch_size": 50, "learning_rate": 1e-3, "dna":False},
+        # {"n_cnn_layers": 3, "conv_kernel_size": 9, "expansion_factor": 2, 
+        #     "nhead": 4, "n_sab_layers": 8, "context_length": 1600, "pos_enc": "relative", 
+        #     "batch_size": 50, "learning_rate": 1e-3, "dna":False},
 
-        {"n_cnn_layers": 4, "conv_kernel_size": 5, "expansion_factor": 2, 
-            "nhead": 8, "n_sab_layers": 4, "context_length": 1200, "pos_enc": "relative", 
-            "batch_size": 50, "learning_rate": 1e-3, "dna":False},
+        # {"n_cnn_layers": 4, "conv_kernel_size": 5, "expansion_factor": 2, 
+        #     "nhead": 8, "n_sab_layers": 4, "context_length": 1200, "pos_enc": "relative", 
+        #     "batch_size": 50, "learning_rate": 1e-3, "dna":False},
+
+        # {"n_cnn_layers": 5, "conv_kernel_size": 3, "expansion_factor": 2, 
+        #     "nhead": 16, "n_sab_layers": 2, "context_length": 800, "pos_enc": "relative", 
+        #     "batch_size": 50, "learning_rate": 1e-3, "dna":False},
 
         {"n_cnn_layers": 5, "conv_kernel_size": 3, "expansion_factor": 2, 
-            "nhead": 16, "n_sab_layers": 2, "context_length": 800, "pos_enc": "relative", 
-            "batch_size": 50, "learning_rate": 1e-3, "dna":False},
-
-        {"n_cnn_layers": 3, "conv_kernel_size": 3, "expansion_factor": 2, 
             "nhead": 8, "n_sab_layers": 8, "context_length": 1600, "pos_enc": "relative", 
             "batch_size": 50, "learning_rate": 1e-3, "dna":False},
         ]
