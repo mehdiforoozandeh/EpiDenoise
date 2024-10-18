f126531fe66c247ca55fa9e1db9fe5796c9b968f
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-10-18 13:14:33 -0700

    ...

--- SAGA.py
+++ SAGA.py
@@ -379,8 +379,8 @@ def main():
     saga.save_latent_representations(Z, latent_file)
     
     # Perform clustering
-    # labels = saga.cluster(Z, algorithm='HMM', n_components=number_of_states)
-    labels = saga.cluster(Z, algorithm='GMM', n_components=number_of_states)
+    labels = saga.cluster(Z, algorithm='HMM', n_components=number_of_states, pca_components=10)
+    # labels = saga.cluster(Z, algorithm='GMM', n_components=number_of_states, pca_components=10)
     # labels = saga.cluster(Z, algorithm='kmeans', n_clusters=number_of_states, pca_components=10)
 
     unique_labels, counts = np.unique(labels, return_counts=True)
