032280777235638e9e6dba1f8cf5f9b5cf2bd090
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-10-18 11:14:45 -0700

    ...

--- SAGA.py
+++ SAGA.py
@@ -150,6 +150,7 @@ class SAGA(object):
             loader = CANDI_LOADER(model, self.hyper_parameters, DNA=self.DNA)
             self.model = loader.load_CANDI()
 
+        self.d_model = self.model.d_model
         print(self.context_length)
         exit()
         self.model = self.model.to(self.device)
