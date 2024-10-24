782dab3741bfe4c69de706afef65373b438c8df2
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-10-23 22:17:31 -0700

    ...

--- SAGA.py
+++ SAGA.py
@@ -549,9 +549,9 @@ def visualize_latent(latent_file, annotation_bed_file=None, output_dir="output",
         plot_and_save(umap_result, labels, 'UMAP of Latent Representations', f'{output_dir}/{bios_name}_umap.png')
 
     if visualize_method == "all" or visualize_method == "tsne":
-        tsne = TSNE(n_components=2, random_state=42)
-        tsne_result = tsne.fit_transform(Z)
-        plot_and_save(tsne_result, labels, 't-SNE of Latent Representations', f'{output_dir}/{bios_name}_tsne.png')
+        # tsne = TSNE(n_components=2, random_state=42)
+        # tsne_result = tsne.fit_transform(Z)
+        # plot_and_save(tsne_result, labels, 't-SNE of Latent Representations', f'{output_dir}/{bios_name}_tsne.png')
 
 def linear_probe_evaluation(latent_file, annotation_bed_file, k_folds=5):
     # Load the latent representations
@@ -727,10 +727,10 @@ def cluster_and_visualize_latent(latent_file, number_of_states=6, transition_exp
                       f'{output_dir}/{bios_name}_umap_{method.lower()}.png')
 
         # t-SNE
-        tsne = TSNE(n_components=2, random_state=42)
-        tsne_result = tsne.fit_transform(Z)
-        plot_and_save(tsne_result, labels, f't-SNE of Latent Representations ({method} clusters)', 
-                      f'{output_dir}/{bios_name}_tsne_{method.lower()}.png')
+        # tsne = TSNE(n_components=2, random_state=42)
+        # tsne_result = tsne.fit_transform(Z)
+        # plot_and_save(tsne_result, labels, f't-SNE of Latent Representations ({method} clusters)', 
+        #               f'{output_dir}/{bios_name}_tsne_{method.lower()}.png')
 
     print("\nClustering and visualization complete.")
 
