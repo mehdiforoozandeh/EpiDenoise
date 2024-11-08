a739c1a13f68bded513e3116c227e79d481a06dd
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-11-08 13:47:38 -0800

    ...

--- data.py
+++ data.py
@@ -560,12 +560,11 @@ class GET_DATA(object):
         # i = df1_ind
         to_download_bios = []
 
+        i = self.DF1[self.DF1['Accession'] == bios].index[0]
+
         bios_data =  requests.get(f"""https://www.encodeproject.org/biosamples/{bios}""", headers=self.headers)
         bios_data = bios_data.json()
         donor_info = extract_donor_information(bios_data)
-
-        print(bios_data)
-        exit()
 
         if os.path.exists(metadata_file_path + "/" + bios + "/") == False:
             os.mkdir(metadata_file_path + "/" + bios + "/")
