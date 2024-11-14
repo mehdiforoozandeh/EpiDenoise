99ffe7acea246a8b42bee21a9161cf09a3de6c80
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-11-14 14:18:52 -0800

    ...

--- data.py
+++ data.py
@@ -1777,10 +1777,15 @@ class ExtendedEncodeDataHandler:
             all_replicates = [rep.split(',') for rep in unique_replicates]
             # Flatten the list of lists into a single list of unique replicates
             unique_replicates = list(set(item for sublist in all_replicates for item in sublist))
-            print(group_df)
+            # print(group_df)
             print(unique_replicates)
+            for replicate in unique_replicates:
+                subset_df = group_df[group_df['accession'] == replicate]
+                print(subset_df)
             print("\n\n")
-            
+
+
+
 
     def init_eic(self, target_split="train"):
         eic_nav_path = os.path.join(self.base_path, "navigation_eic.json")
