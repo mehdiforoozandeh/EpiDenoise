d8a242add640e7722f0950645469e0195f78f338
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-11-21 10:18:24 -0800

    ...

--- data.py
+++ data.py
@@ -2167,8 +2167,6 @@ class ExtendedEncodeDataHandler:
         print(f"Test samples: {test_count} ({test_count/total_count:.1%})")
         print(f"RNA-seq samples in test: {len(rna_seq_keys)}")
 
-        
-
     def train_val_test_split(self, splits=(0.7, 0.15, 0.15), random_seed=42):
         if os.path.exists(self.split_path):
             with open(self.split_path, 'r') as file:
