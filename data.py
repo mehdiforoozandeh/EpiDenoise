756ea94d2a5703c34cc8183549a98b70d3f41d23
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-11-25 13:28:56 -0800

    ...

--- data.py
+++ data.py
@@ -2831,12 +2831,11 @@ class ExtendedEncodeDataHandler:
 
                 unique_exp[exp] += 1
 
-        self.signal_dim = sum(1 for value in unique_exp.values() if value > 0)
+        self.signal_dim = len(self.aliases["experiment_aliases"])
 
-        print(unique_exp)
-        print(self.aliases["experiment_aliases"])
-        print(f"signal_dim: {self.signal_dim}")
-        exit()
+        # print(self.aliases["experiment_aliases"])
+        # print(f"signal_dim: {self.signal_dim}")
+        # exit()
 
         # filter biosamples
         for bios in list(self.navigation.keys()):
