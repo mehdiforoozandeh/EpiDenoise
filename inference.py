e69390def66e3ef90f95e1676c6abbd1e93b8dd2
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-11-25 12:11:32 -0800

    ...

--- inference.py
+++ inference.py
@@ -507,12 +507,12 @@ class CANDIPredictor:
 
 
 def test():
-    model_path = "models/CANDIeic_DNA_random_mask_Nov19_model_checkpoint_epoch10.pth"
-    hyper_parameters_path = "models/hyper_parameters_CANDIeic_DNA_random_mask_Nov19_20241122012038_params45093285.pkl"
+    # model_path = "models/CANDIeic_DNA_random_mask_Nov19_model_checkpoint_epoch10.pth"
+    # hyper_parameters_path = "models/hyper_parameters_CANDIeic_DNA_random_mask_Nov19_20241122012038_params45093285.pkl"
 
 
-    # model_path = "models/CANDIfull_DNA_random_mask_Nov21_model_checkpoint_epoch2.pth"
-    # hyper_parameters_path = "models/hyper_parameters_CANDIfull_DNA_random_mask_Nov21_20241122144620_params3739658.pkl"
+    model_path = "models/CANDIfull_DNA_random_mask_Nov21_model_checkpoint_epoch2.pth"
+    hyper_parameters_path = "models/hyper_parameters_CANDIfull_DNA_random_mask_Nov21_20241122144620_params3739658.pkl"
 
 
     dataset_path = "/project/compbio-lab/encode_data/"
