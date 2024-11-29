34534d14843d2cd78cb0d81f293d16d6cd7fba5a
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-11-28 20:22:24 -0800

    ...

--- inference.py
+++ inference.py
@@ -682,7 +682,7 @@ if __name__ == "__main__":
     
     from difflib import SequenceMatcher
 
-    def similar(a, b, threshold=0.7):
+    def similar(a, b, threshold=0.5):
         return SequenceMatcher(None, a.lower(), b.lower()).ratio() > threshold
 
     # Find exact and similar matches
