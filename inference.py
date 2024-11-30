82967db0653bdbf01bb016cda2511185bc3581bd
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-11-30 12:46:51 -0800

    ...

--- inference.py
+++ inference.py
@@ -1038,7 +1038,7 @@ def train_chromatin_state_probe(
                         except:
                             print(start, end, end - start)
                             print(region[1], region[2], region[2] - region[1])
-                            print(x_input.shapes, seq_input.shapes, mx_input.shapes)
+                            print(x_input.shape, seq_input.shapes, mx_input.shapes)
 
                     z = z.cpu()
 
