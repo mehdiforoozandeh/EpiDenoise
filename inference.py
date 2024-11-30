17507f0caf30cae64d3e3236d07ac09ecd9316b5
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-11-29 21:39:49 -0800

    ...

--- inference.py
+++ inference.py
@@ -829,9 +829,9 @@ def train_chromatin_state_probe(
     probe = ChromatinStateProbe(candi.model.d_model, 18)
 
     splits = chromatin_state_dataset_eic_train_test_val_split(dataset_path)
-    splits["train"] = splits["train"][:5]
-    splits["test"] = splits["test"][:2]
-    splits["val"] = splits["train"][:2]
+    splits["train"] = splits["train"][:2]
+    splits["test"] = splits["test"][:1]
+    splits["val"] = splits["train"][:1]
     
     def prepare_data(split, chrs, num_regions):
         chromatin_state_data = {}
