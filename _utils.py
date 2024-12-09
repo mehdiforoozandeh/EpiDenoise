03c3d0813c75866bfcea8a435aab10397f32fa17
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-12-08 18:14:03 -0800

    ...

--- _utils.py
+++ _utils.py
@@ -1336,6 +1336,8 @@ class COORD(object):
             self.bg_bins = pd.read_csv(f"{self.outdir}/background_bins_{self.resolution}bp.csv").drop("Unnamed: 0", axis=1)
 
         print(f"number of foreground bins: {len(self.fg_bins)} | number of background bins: {len(self.bg_bins)}")
+
+
 if __name__ == "__main__":
 
     # solar_path = "/project/compbio-lab/EIC/"
