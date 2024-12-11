35e13223a3bc1f539c5e5c8221dfcaabbc849a5a
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2024-12-10 16:08:44 -0800

    ...

--- inference.py
+++ inference.py
@@ -1644,6 +1644,6 @@ if __name__ == "__main__":
         candi.chr = "chr21"
         X, Y, P, seq, mX, mY, avX, avY = candi.load_bios(bios_name, x_dsf=1)
         
-        print(candi.navigation.keys())
         exit()
+        print(candi.dataset.navigation.keys())
         metrics = candi.evaluate_leave_one_out(X, mX, mY, avX, Y, P, seq=seq, crop_edges=True, return_preds=False)
