d4c0a3c3d1a5fec958ab2208a937ed3f9de3f803
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-01-03 21:40:29 -0500

    ...

--- eval.py
+++ eval.py
@@ -2306,13 +2306,13 @@ class EVAL_CANDI(object):
             # "count_error_std_hexbin": self.viz.count_error_std_hexbin,
             # "signal_error_std_hexbin": self.viz.signal_error_std_hexbin,
 
-            # "count_scatter_with_marginals": self.viz.count_scatter_with_marginals,
-            # "signal_scatter_with_marginals": self.viz.signal_scatter_with_marginals,
+            "count_scatter_with_marginals": self.viz.count_scatter_with_marginals,
+            "signal_scatter_with_marginals": self.viz.signal_scatter_with_marginals,
 
             # "count_heatmap": self.viz.count_heatmap,
             # "signal_heatmap": self.viz.signal_heatmap,
             
-            # "count_rank_heatmap": self.viz.count_rank_heatmap,
+            "count_rank_heatmap": self.viz.count_rank_heatmap,
             "signal_rank_heatmap": self.viz.signal_rank_heatmap,
 
             # "quantile_hist": self.viz.quantile_hist,
