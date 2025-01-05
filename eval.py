e5aa2ca36eaaf5a1c434d85bf5213973b0a96dc9
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-01-05 17:02:00 -0500

    ...

--- eval.py
+++ eval.py
@@ -1291,10 +1291,15 @@ class VISUALS_CANDI(object):
                     #     vmin = 1e-6
                         
                     norm = LogNorm(vmin=min_val, vmax=max_val)  # Adjust color scaling
+                    # ax.imshow(
+                    #     h, interpolation='nearest', origin='lower',
+                    #     extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]],
+                    #     aspect='auto', cmap=cmocean.cm.deep, norm=norm
+                    # )
                     ax.imshow(
                         h, interpolation='nearest', origin='lower',
                         extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]],
-                        aspect='auto', cmap=cmocean.cm.deep, norm=norm
+                        aspect='auto', cmap=cmocean.cm.deep
                     )
                 except Exception as e:
                     print(f"Failed to plot: {str(e)}")
@@ -1368,11 +1373,16 @@ class VISUALS_CANDI(object):
                     max_val = np.max(h)
                     min_val = np.min(h)
                         
-                    norm = LogNorm(vmin=min_val, vmax=max_val)  # Adjust color scaling
+                    # norm = LogNorm(vmin=min_val, vmax=max_val)  # Adjust color scaling
+                    # ax.imshow(
+                    #     h, interpolation='nearest', origin='lower',
+                    #     extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]],
+                    #     aspect='auto', cmap=cmocean.cm.deep, norm=norm
+                    # )
                     ax.imshow(
                         h, interpolation='nearest', origin='lower',
                         extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]],
-                        aspect='auto', cmap=cmocean.cm.deep, norm=norm
+                        aspect='auto', cmap=cmocean.cm.deep
                     )
                 except Exception as e:
                     print(f"Failed to plot: {str(e)}")
