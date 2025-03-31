8599e2a026eccb59a19b125312b973a44b822998
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-03-30 23:36:21 -0400

    ...

--- model.py
+++ model.py
@@ -690,8 +690,8 @@ class PositionalEncoding(nn.Module):
         position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
         pe = torch.zeros(max_len, 1, d_model)
-        pe[:, 0, 0::2] = torch.sin(position * div_term)
-        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        pe[:, 0, 0::2] = torch.sin(position * div_term[:d_model // 2])
+        pe[:, 0, 1::2] = torch.cos(position * div_term[:d_model // 2])
 
         self.pe = torch.permute(pe, (1, 0, 2))
 
