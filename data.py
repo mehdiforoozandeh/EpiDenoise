0c1822059caf710e0b726f24fe186769fc08fd52
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-04-23 11:34:14 -0400

    ...

--- data.py
+++ data.py
@@ -3055,7 +3055,8 @@ class ExtendedEncodeDataHandler:
             'ATAC-seq', 'DNase-seq', 'H2AFZ', 'H2AK5ac', 'H2AK9ac', 'H2BK120ac', 'H2BK12ac', 'H2BK15ac', 
             'H2BK20ac', 'H2BK5ac', 'H3F3A', 'H3K14ac', 'H3K18ac', 'H3K23ac', 'H3K23me2', 'H3K27ac', 'H3K27me3', 
             'H3K36me3', 'H3K4ac', 'H3K4me1', 'H3K4me2', 'H3K4me3', 'H3K56ac', 'H3K79me1', 'H3K79me2', 'H3K9ac', 
-            'H3K9me1', 'H3K9me2', 'H3K9me3', 'H3T11ph', 'H4K12ac', 'H4K20me1', 'H4K5ac', 'H4K8ac', 'H4K91ac'], 
+            'H3K9me1', 'H3K9me2', 'H3K9me3', 'H3T11ph', 'H4K12ac', 'H4K20me1', 'H4K5ac', 'H4K8ac', 'H4K91ac', 
+            'RNA-seq'], 
         excludes=[], 
         # includes = [
         #     "H3K4me3", "H3K36me3", "H3K27me3", "H3K4me1", "H3K9me3", "H3K27ac", "CTCF", 
@@ -3083,10 +3084,10 @@ class ExtendedEncodeDataHandler:
             
             self.merged_train_val_test_split()
 
-            for bios in self.navigation:
-                print(bios, self.navigation[bios].keys(), self.has_rnaseq(bios))
+            # for bios in self.navigation:
+            #     print(bios, self.navigation[bios].keys(), self.has_rnaseq(bios))
 
-            exit()
+            # exit()
 
         else:
             self.train_val_test_split()
