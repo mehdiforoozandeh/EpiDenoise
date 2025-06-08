750b443da5a8fb506ca01cb3738e73f86e4c2954
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-06-08 16:31:09 -0400

    ..

--- CANDI.py
+++ CANDI.py
@@ -903,15 +903,16 @@ class PRETRAIN(object):
                     f"Imp_Pval_PCC {np.mean(batch_rec['imp_pval_pearson']):.2f}",
                     f"Ups_Pval_PCC {np.mean(batch_rec['ups_pval_pearson']):.2f}", "\n",
                     
-                    f"ema_imp_pval_r2 {prog_mon_ema['imp_pval_r2']:.2f}",
-                    f"ema_imp_pval_PCC {prog_mon_ema['imp_pval_pearson']:.2f}",
-                    f"ema_imp_pval_SRCC {prog_mon_ema['imp_pval_spearman']:.2f}", 
-                    f"ema_imp_pval_loss {prog_mon_ema['imp_pval_loss']:.2f}", "\n",
+                    # -1 since we multiplied it to -1 earlier :))
+                    f"EMA_imp_pval_r2 {-1*prog_mon_ema['imp_pval_r2']:.2f}",
+                    f"EMA_imp_pval_PCC {-1*prog_mon_ema['imp_pval_pearson']:.2f}",
+                    f"EMA_imp_pval_SRCC {-1*prog_mon_ema['imp_pval_spearman']:.2f}", 
+                    f"EMA_imp_pval_loss {-1*prog_mon_ema['imp_pval_loss']:.2f}", "\n",
 
-                    f"EMA_imp_count_r2 {prog_mon_ema['imp_count_r2']:.2f}",
-                    f"EMA_imp_count_PCC {prog_mon_ema['imp_count_pearson']:.2f}",
-                    f"EMA_imp_count_SRCC {prog_mon_ema['imp_count_spearman']:.2f}", 
-                    f"EMA_imp_count_loss {prog_mon_ema['imp_count_loss']:.2f}", "\n",
+                    f"EMA_imp_count_r2 {-1*prog_mon_ema['imp_count_r2']:.2f}",
+                    f"EMA_imp_count_PCC {-1*prog_mon_ema['imp_count_pearson']:.2f}",
+                    f"EMA_imp_count_SRCC {-1*prog_mon_ema['imp_count_spearman']:.2f}", 
+                    f"EMA_imp_count_loss {-1*prog_mon_ema['imp_count_loss']:.2f}", "\n",
                     
                     f"took {int(minutes)}:{int(seconds):02d}", 
                     f"Gradient_Norm {np.mean(batch_rec['grad_norm']):.2f}",
