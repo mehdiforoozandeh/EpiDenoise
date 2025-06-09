d98cba9886f60ddba183afe3ad2edb5d3dce8a60
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-06-08 21:29:49 -0400

    ...

--- _utils.py
+++ _utils.py
@@ -528,6 +528,7 @@ class MONITOR_VALIDATION(object): # CANDI
                 except:
                     print(availability)
                     print(self.dataset.navigation[bios_name].keys())
+                    print(self.dataset.aliases["experiment_aliases"])
                     continue
 
                 for comparison in ['imputed', 'upsampled']:
