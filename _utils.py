6f9450601a3801d2920feafbe7147104f8e1f26b
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-06-28 18:20:20 -0400

    ..

--- _utils.py
+++ _utils.py
@@ -641,7 +641,8 @@ class MONITOR_VALIDATION(object): # CANDI
             elif j in list(available_Y_indices):
                 comparison = "imputed"
                 target_count = Y[:, j].numpy()
-                target_pval = P[:, j].numpy()
+                
+            target_pval = P[:, j].numpy()
 
             else:
                 continue
