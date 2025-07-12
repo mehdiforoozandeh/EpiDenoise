495fc149c335bf48d8df96ede638e60ac4197217
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-07-12 10:59:19 -0700

    ...

--- eval.py
+++ eval.py
@@ -473,10 +473,10 @@ class METRICS(object):
             # find cutoff K
             K = nbinom.ppf(1 - epsilon, r_j, p_j)
             if not np.isfinite(K):
-                print(K, r_j, p_j)
                 K = 0
             else:
                 K = int(K)
+                print(K, r_j, p_j)
               
             # PMF/CDF arrays
             k = np.arange(K + 1)
