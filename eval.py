a78e6303d65e8ccc754543d367f1900d34949cd9
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-07-12 11:02:31 -0700

    ...

--- eval.py
+++ eval.py
@@ -2985,7 +2985,7 @@ class EVAL_CANDI(object):
                         'C_MSE-1obs': self.metrics.mse1obs(C_target, pred_count),
                         'C_Spearman_1obs': self.metrics.spearman1_obs(C_target, pred_count),
                         'C_r2_1obs': self.metrics.r2_1obs(C_target, pred_count),
-                        'C_Cidx_1obs':self.metrics.c_index_nbinom_1obs(pred_count_n, pred_count_p, C_target, num_pairs=5000),
+                        # 'C_Cidx_1obs':self.metrics.c_index_nbinom_1obs(pred_count_n, pred_count_p, C_target, num_pairs=5000),
 
                         'C_MSE-1imp': self.metrics.mse1imp(C_target, pred_count),
                         'C_Pearson_1imp': self.metrics.pearson1_imp(C_target, pred_count),
@@ -2996,13 +2996,13 @@ class EVAL_CANDI(object):
                         'C_Pearson_gene': self.metrics.pearson_gene(C_target, pred_count),
                         'C_Spearman_gene': self.metrics.spearman_gene(C_target, pred_count),
                         'C_r2_gene': self.metrics.r2_gene(C_target, pred_count),
-                        # 'C_Cidx_gene':self.metrics.c_index_nbinom_gene(pred_count_n, pred_count_p, C_target, num_pairs=-1),
+                        'C_Cidx_gene':self.metrics.c_index_nbinom_gene(pred_count_n, pred_count_p, C_target, num_pairs=-1),
 
                         'C_MSE-prom': self.metrics.mse_prom(C_target, pred_count),
                         'C_Pearson_prom': self.metrics.pearson_prom(C_target, pred_count),
                         'C_Spearman_prom': self.metrics.spearman_prom(C_target, pred_count),
                         'C_r2_prom': self.metrics.r2_prom(C_target, pred_count),
-                        # 'C_Cidx_prom':self.metrics.c_index_nbinom_prom(pred_count_n, pred_count_p, C_target, num_pairs=-1),
+                        'C_Cidx_prom':self.metrics.c_index_nbinom_prom(pred_count_n, pred_count_p, C_target, num_pairs=-1),
 
                         "C_peak_overlap_01thr": self.metrics.peak_overlap(C_target, pred_count, p=0.01),
                         "C_peak_overlap_05thr": self.metrics.peak_overlap(C_target, pred_count, p=0.05),
