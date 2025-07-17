804e10033eab00f353fcfacfed7dfbf1bb828ed6
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-07-17 12:36:07 -0700

    ..

--- eval.py
+++ eval.py
@@ -3353,7 +3353,8 @@ class EVAL_CANDI(object):
         else:
             DF_True = DF.loc[:, [c for c in DF.columns if "True" in c]]
             DF_Pred = DF.loc[:, [c for c in DF.columns if "Pred" in c]]
-
+            
+            available_assays = { self.expnames[a] for a in availability }
             DF_Pred_Denoised = DF_Pred.loc[:, [c for c in DF_Pred.columns if c in available_assays]]
 
             print(DF.shape)
