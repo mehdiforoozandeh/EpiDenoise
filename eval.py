82c227137e2481c84d6b0bb93aaaab6ee47d905f
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-07-20 11:19:09 -0700

    ..

--- eval.py
+++ eval.py
@@ -4471,7 +4471,7 @@ class EVAL_CANDI(object):
         SAGA_obs.fit(obs_data)
         SAGA_obs_MAP = SAGA_obs.predict(obs_data)
         SAGA_obs_posterior = SAGA_obs.predict_proba(obs_data)
-        print("obs_MAP", {k: float(v)/len(obs_data.shape[0]) for k, v in Counter(SAGA_obs_MAP).items()})
+        print("obs_MAP", {k: float(v)/obs_data.shape[0] for k, v in Counter(SAGA_obs_MAP).items()})
 
         print(f"\nfitting the SAGA on denoised signal (d={len(available_indices)})")
         SAGA_den = SoftMultiAssayHMM(n_components=n_components, n_iter=n_iter, tol=tol, init_params="stmc", params="stmc", random_state=random_state)
@@ -4479,7 +4479,7 @@ class EVAL_CANDI(object):
         SAGA_den_MAP = SAGA_den.predict(den_data)
         SAGA_den_posterior = SAGA_den.predict_proba(den_data)
         # print("den_MAP", Counter(SAGA_den_MAP))
-        print("den_MAP", {k: float(v)/len(obs_data.shape[0]) for k, v in Counter(SAGA_den_MAP).items()})
+        print("den_MAP", {k: float(v)/obs_data.shape[0] for k, v in Counter(SAGA_den_MAP).items()})
         
 
         print(f"\nfitting the SAGA on denoised + imputed signal (d={mu_ups.shape[1]})")
@@ -4488,7 +4488,7 @@ class EVAL_CANDI(object):
         SAGA_denimp_MAP = SAGA_denimp.predict(denimp_data)
         SAGA_denimp_posterior = SAGA_denimp.predict_proba(denimp_data)
         # print("denimp_MAP", Counter(SAGA_denimp_MAP))
-        print("denimp_MAP", {k: float(v)/len(obs_data.shape[0]) for k, v in Counter(SAGA_denimp_MAP).items()})
+        print("denimp_MAP", {k: float(v)/obs_data.shape[0] for k, v in Counter(SAGA_denimp_MAP).items()})
 
         
         # 1. Initialize and fit PCA
@@ -4500,7 +4500,7 @@ class EVAL_CANDI(object):
         SAGA_latent_MAP = SAGA_latent.predict(Z_reduced)
         SAGA_latent_posterior = SAGA_latent.predict_proba(Z_reduced)
         # print("latent_MAP", Counter(SAGA_latent_MAP))
-        print("latent_MAP", {k: float(v)/len(obs_data.shape[0]) for k, v in Counter(SAGA_latent_MAP).items()})
+        print("latent_MAP", {k: float(v)/obs_data.shape[0] for k, v in Counter(SAGA_latent_MAP).items()})
         
 
         print("obs V den -- hard:\n", compare_hard_clusterings(SAGA_obs_MAP, SAGA_den_MAP))
