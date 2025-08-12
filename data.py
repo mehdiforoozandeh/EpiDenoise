722e2862d8a7ef12b1e5d707cb35bb141651233d
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-08-11 22:23:29 -0700

    ...

--- data.py
+++ data.py
@@ -2773,6 +2773,7 @@ class ExtendedEncodeDataHandler:
         elif loci_gen == "gw":
             self.generate_full_chr_loci(context_length, chrs="gw")
         else:
+            print(f"generating {loci_gen} loci")
             self.generate_full_chr_loci(context_length, chrs=loci_gen)
 
         print(f"num loci: {len(self.m_regions)}")
