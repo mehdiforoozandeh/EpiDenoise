4db90606dffe2117c4a687147e2ef807c9967896
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-08-17 12:10:14 -0700

    ...

--- data.py
+++ data.py
@@ -4323,15 +4323,15 @@ if __name__ == "__main__":
             # with open(output_json_path, "w") as out_f:
             #     json.dump(biosample_dict, out_f, indent=2)
 
-        merged_navigation_path = os.path.join(solar_data_path, "merged_navigation.json")
-        output_merged_json = os.path.join(solar_data_path, "download_plan_merged_navigation.json")
-        build_exp_dict(merged_navigation_path, solar_data_path, output_merged_json)
-        print(f"Saved experiment plan for merged_navigation.json to {output_merged_json}")
+        # merged_navigation_path = os.path.join(solar_data_path, "merged_navigation.json")
+        # output_merged_json = os.path.join(solar_data_path, "download_plan_merged_navigation.json")
+        # build_exp_dict(merged_navigation_path, solar_data_path, output_merged_json)
+        # print(f"Saved experiment plan for merged_navigation.json to {output_merged_json}")
     
-        # eic_csv_path = os.path.join(solar_data_path, "EIC_experiments.csv")
-        # output_eic_json = os.path.join(solar_data_path, "download_plan_eic_experiments.json")
-        # build_eic_exp_dict(eic_csv_path, output_eic_json)
-        # print(f"✅ Saved EIC experiment download plan to {output_eic_json}")
+        eic_csv_path = os.path.join(solar_data_path, "EIC_experiments.csv")
+        output_eic_json = os.path.join(solar_data_path, "download_plan_eic_experiments.json")
+        build_eic_exp_dict(eic_csv_path, output_eic_json)
+        print(f"✅ Saved EIC experiment download plan to {output_eic_json}")
     
     else:
         d = GET_DATA()
